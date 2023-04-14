e3432df87521e5f32636d27fd2ad8079a2cec1a3
amacaulay <adam.macaulay@charliehealth.com>
2023-04-14 15:04:28 -0600

    foot start

--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '374099499c8f0962185e');
+<?php return array('dependencies' => array(), 'version' => '2ae2c0b6669432f974fa');
--- wp-content/themes/charliehealth/footer.php
+++ wp-content/themes/charliehealth/footer.php
@@ -1,8 +1,28 @@
-	<footer>
-	</footer>
+<footer class="grid bg-darkest-blue gap-y-sp-16 container-padding">
+	<!-- menu -->
+	<div class="container grid lg:grid-cols-5 gap-sp-16">
+		<div class="flex flex-col">
+			<h4 class="text-white">Our Program</h4>
+			<a href="#" class="text-white text-h5">Intensive Outpatient Program (IOP)</a>
+			<a href="#" class="text-white text-h5">Intensive Outpatient Program (IOP)</a>
+			<a href="#" class="text-white text-h5">Intensive Outpatient Program (IOP)</a>
+			<a href="#" class="text-white text-h5">Intensive Outpatient Program (IOP)</a>
+		</div>
+		<div></div>
+		<div></div>
+	</div>
+	<div class="container w-full">
+		<div class="flex flex-col justify-between rounded-md lg:flex-row bg-lightest-teal py-sp-5 px-sp-8">
+			<p class="mb-0">*If this is a life-threatening emergency, please call 911 or the <a href="https://988lifeline.org/" target="_blank">National Suicide Prevention Lifeline</a></p><a href="tel:+988" class="flex items-center gap-sp-4"><img src="https://assets-global.website-files.com/62daf9ae3616b86eec143652/62fe6bbb2e137b527c8f80d0_phone%20call.svg" width="25" alt="phone call icon">
+				<div>988</div>
+			</a>
+		</div>
+	</div>
+</footer>
 <!-- </div> -->
 
-	<?php wp_footer(); ?>
-	<pre class="fixed left-0 right-0 w-full text-xs text-center text-white bottom-4 -z-10 opacity-30">Made with ❤️ in Denver</pre>
+<?php wp_footer(); ?>
+<pre class="fixed left-0 right-0 w-full text-xs text-center text-white bottom-4 -z-10 opacity-30">Made with ❤️ in Denver</pre>
 </body>
+
 </html>
