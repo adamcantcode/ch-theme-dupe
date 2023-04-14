e8a14fe83d43053f0aeadbc590a3f111c08a6a48
amacaulay <adam.macaulay@charliehealth.com>
2023-04-14 08:45:12 -0600

    who we serve

--- wp-content/themes/charliehealth/header.php
+++ wp-content/themes/charliehealth/header.php
@@ -20,7 +20,7 @@
     <nav class="container px-5 relative flex items-center justify-between min-h-[68px]">
       <div class="nav-logo">
         <a href="#">
-          <?php include('resources/images/ch-logo.php') ?>
+          <?php include('resources/images/ch-logo.php'); ?>
         </a>
       </div>
       <div class="slide-out overflow-y-scroll lg:overflow-y-visible fixed bottom-0 lg:top-0 bg-darkest-blue lg:bg-transparent lg:flex lg:relative lg:h-auto h-[calc(100vh-68px)] <?= is_user_logged_in() ? 'top-[116px]' : 'top-[68px]'; ?>">
