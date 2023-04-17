a6cbb9cdc4ca8cdb6576a6e809f4b0f62fe7b1c3
amacaulay <adam.macaulay@charliehealth.com>
2023-04-16 19:25:34 -0600

    add image placeholder helepr

---
+++ wp-content/themes/charliehealth/helpers/helper-functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function placeHolderImage($width = 500, $height = 500)
+{
+  $placeHolderImg = "https://images.placeholders.dev/?width=$width&height=$height&text=FPO";
+  return $placeHolderImg;
+}
