ca52e595542aee17a10e435dd59545ebfafd14f0
amacaulay <adam.macaulay@charliehealth.com>
2023-04-21 13:08:02 -0600

    add divider

--- wp-content/themes/charliehealth/blocks/section-container/section-container.php
+++ wp-content/themes/charliehealth/blocks/section-container/section-container.php
@@ -4,6 +4,8 @@
 include(get_template_directory() . '/helpers/spacing.php');
 
 $containerWidth = get_field('container_width') ? '-' . get_field('container_width') : '';
+
+// var_dump($containerWidth);
 ?>
 
 <section <?= $anchor; ?>class="<?= $blockClasses; ?>">
