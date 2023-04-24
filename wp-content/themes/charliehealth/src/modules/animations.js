184f815a7cac101904210a577cc1cc454f029ce2
amacaulay <adam.macaulay@charliehealth.com>
2023-04-23 20:39:36 -0600

    fix mobile text wrap

--- wp-content/themes/charliehealth/src/modules/animations.js
+++ wp-content/themes/charliehealth/src/modules/animations.js
@@ -1,7 +1,7 @@
 /**
  * Man nav hover actions
  */
-import { gsap } from "gsap";
+import { gsap } from 'gsap';
 
 export default function animations() {
   var topLevelLinks = document.querySelectorAll('.nav-parent-menu');
@@ -12,22 +12,24 @@ export default function animations() {
     const linksTimeLine = gsap.timeline();
 
     // Add an animation to the timeline to animate the opacity and x position of the sub-links
-    linksTimeLine.to(
-      link.lastElementChild.querySelectorAll('.nav-link.sub-link'),
-      {
-        opacity: 1,
-        y: 0,
-        stagger: 0.025,
-      }
-    );
+    if (
+      link.lastElementChild.querySelectorAll('.nav-link.sub-link').length > 0
+    ) {
+      linksTimeLine.to(
+        link.lastElementChild.querySelectorAll('.nav-link.sub-link'),
+        {
+          opacity: 1,
+          y: 0,
+          stagger: 0.025,
+        }
+      );
+    }
     // Add a mouseenter event listener to the current top level link
     link.addEventListener('mouseenter', () => {
       // If the timeline is not already active, play it from the beginning
       if (!linksTimeLine.isActive()) {
-        console.log('not active');
         linksTimeLine.play(0);
       } else {
-        console.log('active');
       }
     });
   });
--- wp-content/themes/charliehealth/src/modules/mobile-nav.js
+++ wp-content/themes/charliehealth/src/modules/mobile-nav.js
@@ -2,15 +2,21 @@
  * Main nav hover actions
  */
 export default function mobileNav() {
-  var menu = document.querySelector('.open-close');
-  var slideOut = document.querySelector('.slide-out');
-  var mainContent = document.querySelector('.site-main');
-  var topLevelLinks = document.querySelectorAll('.nav-parent-menu > .nav-link:not(.static)');
+  const menu = document.querySelector('.open-close');
+  const slideOut = document.querySelector('.slide-out');
+  const mainContent = document.querySelector('.site-main');
+  const topLevelLinks = document.querySelectorAll(
+    '.nav-parent-menu > .nav-link:not(.static)'
+  );
 
   /**
    * On click, toggle active.
    */
   menu.addEventListener('click', () => {
+    var menuText = menu.firstChild.nextElementSibling;
+    menuText.innerHTML === 'Menu'
+      ? (menuText.innerHTML = 'Close')
+      : (menuText.innerHTML = 'Menu');
     menu.classList.toggle('active');
     slideOut.classList.toggle('active');
     mainContent.classList.toggle('active');
@@ -28,7 +34,7 @@ export default function mobileNav() {
 
   /**
    * Remove active from all submenus except the current.
-   * @param {string} activeLink 
+   * @param {string} activeLink
    */
   const removeActive = (activeLink) => {
     topLevelLinks.forEach((link) => {
