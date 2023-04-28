517fbe7e8e421f01b2514df66e81a2e93d8a2cb6
amacaulay <adam.macaulay@charliehealth.com>
2023-04-28 15:30:13 -0600

    rename cats to audiences

--- wp-content/themes/charliehealth/functions.php
+++ wp-content/themes/charliehealth/functions.php
@@ -125,9 +125,10 @@ add_filter('request', function ($request) {
   return $request;
 });
 
-function remove_page_slug_trailing_slash($redirect_url) {
+function remove_page_slug_trailing_slash($redirect_url)
+{
   if (is_page('search')) {
-      $redirect_url = untrailingslashit($redirect_url);
+    $redirect_url = untrailingslashit($redirect_url);
   }
   return $redirect_url;
 }
@@ -145,3 +146,39 @@ add_filter('redirect_canonical', 'remove_page_slug_trailing_slash');
 //   }
 // }
 // add_action('template_redirect', 'change_search_url_rewrite');
+
+/**
+ * Rename Category to Theme
+ *
+ */
+function be_rename_category_theme()
+{
+
+  $singular_name = 'Audience';
+  $plural_name = 'Audiences';
+
+  $labels = array(
+    'name'          => $plural_name,
+    'menu_name'        => $plural_name,
+    'singular_name'      => $singular_name,
+    'search_items'      => 'Search ' . $plural_name,
+    'popular_items'      => 'Popular ' . $plural_name,
+    'all_items'        => 'All ' . $plural_name,
+    'parent_item'        => 'Parent ' . $singular_name,
+    'parent_item_colon'    => 'Parent ' . $singular_name . ':',
+    'edit_item'        => 'Edit ' . $singular_name,
+    'view_item'        => 'View ' . $singular_name,
+    'update_item'        => 'Update ' . $singular_name,
+    'add_new_item'      => 'Add New ' . $singular_name,
+    'new_item_name'      => 'New ' . $singular_name . ' Name',
+    'separate_items_with_commas' => 'Separate ' . $plural_name . ' with commas',
+    'add_or_remove_items'    => 'Add or remove ' . $plural_name,
+    'back_to_items'      => '← Back to ' . $plural_name,
+    'items_list_navigation'   => $plural_name . ' list navigation',
+    'items_list'        => $plural_name . ' list',
+  );
+
+  global $wp_taxonomies;
+  $wp_taxonomies['category']->labels = (object) array_merge((array) $wp_taxonomies['category']->labels, $labels);
+}
+add_action('init', 'be_rename_category_theme');
