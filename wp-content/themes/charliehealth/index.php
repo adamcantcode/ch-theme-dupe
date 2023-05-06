e984a2c4b1bda095e2fa72ee69246e93b33d78dd
amacaulay <adam.macaulay@charliehealth.com>
2023-05-06 12:25:35 -0600

    add icons

--- wp-content/themes/charliehealth/index.php
+++ wp-content/themes/charliehealth/index.php
@@ -1,19 +1,7 @@
 <?php get_header(); ?>
 
 <main id="primary" class="site-main lg:mt-[68px] mt-0">
-  <section class="section">
-    <div class="container">
-      <?php
-      if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-          <?= the_content(); ?>
-      <?php endwhile;
-      else :
-        _e('Sorry, no posts were found.', 'textdomain');
-      endif;
-      ?>
-    </div>
-  </section>
+  <?= the_content(); ?>
 </main>
 
 <?php get_footer(); ?>
