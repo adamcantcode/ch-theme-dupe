ae8cffc1ab45fedd4165a4e1bac4f0dc4d6002a9
amacaulay <adam.macaulay@charliehealth.com>
2023-05-15 19:47:37 -0600

    layout

--- wp-content/themes/charliehealth/single-referral.php
+++ wp-content/themes/charliehealth/single-referral.php
@@ -21,7 +21,22 @@ $references = get_field('references') ?: '';
       <p>Need help or additional information? Please contact our outreach and admissions teams below for any questions about referring patients to our programs or about the admissions process.</p>
       <p><i>CH MH Services (CA) LLC is certified by the State Department of Health Care Services (Lic. #300414AP expiring 06/30/2023).</i></p>
       <p><i>For detailed information on our California Facility Licensure, please visit the <a href="https://geohub-cadhcs.hub.arcgis.com/datasets/CADHCS::sud-recovery-treatment-facilities-1/explore?location=33.650768%2C-117.838283%2C21.00" target="_blank">California Health and Human Services Department's website.</a></i></p>
-      <div class="divider my-sp-16"></div>
+    </div>
+  </section>
+  <section class="section-horizontal">
+    <div class="container-md">
+      <div class="divider"></div>
+    </div>
+  </section>
+  <section class="section-top">
+    <div class="container-md">
+      <div class="formmm">
+        [FORM]
+      </div>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container-md">
       <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-sp-16 gap-sp-8">
         <div>
           <h4>For general questions about Charlie Health's services and programs, please contact:</h4>
