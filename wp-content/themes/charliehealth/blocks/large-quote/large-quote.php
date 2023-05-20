39446a56aa4a0f4660ac983b575916770332df90
amacaulay <adam.macaulay@charliehealth.com>
2023-05-20 17:40:51 -0600

    update quote block

--- wp-content/themes/charliehealth/blocks/large-quote/large-quote.php
+++ wp-content/themes/charliehealth/blocks/large-quote/large-quote.php
@@ -1,24 +1,30 @@
 <?php
 $person = get_field('person');
 $quote = get_field('quote');
-$image = get_field('image');
+$customImage = get_field('custom_image');
 $reverse = get_field('direction');
 $order = 'lg:order-1';
 $imageOrder = 'lg:order-2';
 $grid = 'lg:grid-cols-[1.33fr_1fr]';
+
 
 if ($reverse) {
   $order = 'lg:order-2';
   $imageOrder = 'lg:order-1';
   $grid = 'lg:grid-cols-[1fr_1.33fr]';
 }
-if (!$image) {
+if ($customImage) {
+  $image = $customImage['url'];
+  $alt = $customImage['alt'];
+} else {
   $image = get_the_post_thumbnail_url($person);
+  $thumbnailID = get_post_thumbnail_id($person);
+  $alt = get_post_meta($thumbnailID, '_wp_attachment_image_alt', true);
 }
 ?>
 <div class="grid items-center grid-cols-1 lg:gap-sp-16 gap-sp-8 <?= $grid; ?>">
   <div class="<?= $imageOrder; ?>">
-    <img src="<?= $image; ?>" alt="Photo of Justin Weiss" class="object-cover w-full rounded-md aspect-square">
+    <img src="<?= $image; ?>" alt="<?= $alt; ?>" class="object-cover w-full rounded-md aspect-square">
   </div>
   <div class="<?= $order; ?>">
     <h3 class="mb-sp-1"><?= get_the_title($person); ?></h3>
