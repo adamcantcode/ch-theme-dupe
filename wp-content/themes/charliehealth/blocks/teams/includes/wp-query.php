1f1d9238f349d1374a998407491ba5b598bb552f
amacaulay <adam.macaulay@charliehealth.com>
2023-05-24 09:43:30 -0600

    team block updates

---
+++ wp-content/themes/charliehealth/blocks/teams/includes/wp-query.php
@@ -0,0 +1,24 @@
+<?php
+if ($entireDivision) {
+  $postID = get_the_id();
+} else {
+  $postID = $post;
+}
+$title = get_field('job_title', $postID);
+?>
+<div class="grid justify-items-start gap-sp-1">
+  <div class="cursor-pointer" data-modal-id="<?= get_the_id($postID); ?>">
+    <img src="<?= get_the_post_thumbnail_url($postID); ?>" alt="" class="rounded-[50%] mb-5">
+    <h4><?= get_the_title($postID); ?></h4>
+  </div>
+  <?php /*; ?>
+  <h5><?= $title; ?></h5>
+  <h5><?= $state; ?></h5>
+  <a href="tel:+<?= $phone; ?>" class="inline-block no-underline break-all">
+    <h5><?= $phone; ?></h5>
+  </a>
+  <a href="mailto:<?= $email; ?>" class="inline-block no-underline break-all">
+    <h5><?= $email; ?></h5>
+  </a>
+  <?php */ ?>
+</div>
--- wp-content/themes/charliehealth/blocks/teams/teams.php
+++ wp-content/themes/charliehealth/blocks/teams/teams.php
@@ -19,44 +19,19 @@ $heading = get_field('heading');
         'meta_value'  => $division
       );
       $query = new WP_Query($args);
-
-      $queryPosts = $query->have_posts();
-      $getPosts = $query->get_posts();
-      $queryThePosts = $query->the_post();
+      if ($query->have_posts()) :
+        while ($query->have_posts()) :
+          $query->the_post();
+          include('includes/wp-query.php');
+        endwhile;
+      endif;
     } else {
-      $queryPosts = $posts;
-      $getPosts = $posts;
-      $queryThePosts = null;
+      if ($posts) :
+        foreach ($posts as $post) :
+          include('includes/wp-query.php');
+        endforeach;
+      endif;
     }
-
-    if ($queryPosts) : foreach ($getPosts as $post) : $queryThePosts;
-        if ($entireDivision) {
-          $postID = get_the_id();
-        } else {
-          $postID = $post;
-        }
-        $title = get_field('job_title', $postID);
-    ?>
-        <div class="grid justify-items-start gap-sp-1">
-          <div class="cursor-pointer" data-modal-id="<?= get_the_id($postID); ?>">
-            <img src="<?= get_the_post_thumbnail_url($postID); ?>" alt="" class="rounded-[50%] mb-5">
-            <h4><?= get_the_title($postID); ?></h4>
-          </div>
-          <h5><?= $title; ?></h5>
-          <h5><?= $state; ?></h5>
-          <a href="tel:+<?= $phone; ?>" class="inline-block no-underline break-all">
-            <h5><?= $phone; ?></h5>
-          </a>
-          <a href="mailto:<?= $email; ?>" class="inline-block no-underline break-all">
-            <h5><?= $email; ?></h5>
-          </a>
-        </div>
-    <?php
-      endforeach;
-      wp_reset_postdata();
-    else :
-      echo 'No posts found';
-    endif;
     ?>
   </div>
 </div>
