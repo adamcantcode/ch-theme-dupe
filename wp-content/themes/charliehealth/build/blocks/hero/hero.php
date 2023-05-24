3b77935cdbec9ae35cdd95fb63052a833a430c9c
amacaulay <adam.macaulay@charliehealth.com>
2023-05-24 14:29:49 -0600

    cover image hero

--- wp-content/themes/charliehealth/build/blocks/hero/hero.php
+++ wp-content/themes/charliehealth/build/blocks/hero/hero.php
@@ -8,7 +8,6 @@ $subtitle = get_field('hero_subtitle');
 $subtitleEditor = get_field('hero_subtitle_editor');
 $heroImage = get_field('hero_image');
 $icon = get_field('hero_icon');
-// $test = get_fields();
 
 if ($style === 'image') {
   $titleClass = 'text-display mb-sp-8 hidden lg:block';
@@ -33,7 +32,9 @@ if ($style === 'image') {
       <h1 class="hidden text-display mb-sp-8 lg:block"><?= $title; ?></h1>
       <div class="flex items-center gap-sp-3 mb-sp-8">
         <!-- TODO Update image -->
-        <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="" class="w-10">
+        <?php if ($icon) : ?>
+          <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="" class="w-10">
+        <?php endif; ?>
         <h3 class="mb-0"><?= $subtitle; ?></h3>
       </div>
       <?php include(get_template_directory() . '/includes/button-group.php'); ?>
@@ -62,6 +63,28 @@ if ($style === 'image') {
     <div class="grid items-center lg:grid-cols-2">
       <img src="<?= $heroImage['url'] ?: placeHolderImage(600, 600); ?>" alt="<?= $heroImage['url'] ?: 'Placeholder image'; ?>" class="object-cover lg:rounded-l-md lg:rounded-tr-none rounded-t-md min-h-full lg:h-[400px] h-[200px] w-full">
       <div class="p-sp-8">
+        <?= $subtitleEditor; ?>
+        <?php include(get_template_directory() . '/includes/button-group.php'); ?>
+      </div>
+    </div>
+  </div>
+<?php endif; ?>
+<?php if ($style === 'cover') : ?>
+  <div class="">
+    <div class="relative lg:h-[500px] h-[200px]">
+      <div class="absolute inset-0">
+        <img src="<?= $heroImage['url'] ?: placeHolderImage(600, 600); ?>" alt="<?= $heroImage['url'] ?: 'Placeholder image'; ?>" class="object-cover w-full h-full rounded-lg">
+      </div>
+      <h1 class="absolute text-white -translate-y-1/2 top-1/2 left-sp-8"><?= $title; ?></h1>
+    </div>
+    <div class="grid items-start lg:gap-sp-16 gap-sp-4 lg:grid-cols-2 mt-sp-12">
+      <div class="flex items-center gap-sp-3">
+        <?php if ($icon) : ?>
+          <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="" class="w-10">
+        <?php endif; ?>
+        <h3 class="mb-0"><?= $subtitle; ?></h3>
+      </div>
+      <div class="">
         <?= $subtitleEditor; ?>
         <?php include(get_template_directory() . '/includes/button-group.php'); ?>
       </div>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'd8699a81f45caf2404dc');
+<?php return array('dependencies' => array(), 'version' => '582bb3edb6dd391bfee3');
