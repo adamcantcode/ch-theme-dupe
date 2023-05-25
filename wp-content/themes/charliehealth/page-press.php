bf221f2b5dce1f2c1547cb92c43bd209d28a6352
amacaulay <adam.macaulay@charliehealth.com>
2023-05-24 20:21:43 -0600

    update press

--- wp-content/themes/charliehealth/page-press.php
+++ wp-content/themes/charliehealth/page-press.php
@@ -23,15 +23,18 @@ Template Name: Press page
     </section>
     <section class="section">
       <div class="container">
-        <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10 mb-sp-10">
+        <div class="grid lg:grid-cols-3 gap-x-sp-8 gap-y-sp-10 mb-sp-10">
           <?php
           $args = array(
-            'post_type' => 'press',
+            'post_type'      => 'press',
             'posts_per_page' => 3,
-            'meta_query' => array(
+            'meta_key'       => 'date',
+            'orderby'        => 'meta_value',
+            'order'          => 'DESC',
+            'meta_query'     => array(
               array(
-                'key' => 'featured',
-                'value' => true,
+                'key'     => 'featured',
+                'value'   => true,
                 'compare' => '=',
               )
             )
@@ -41,12 +44,24 @@ Template Name: Press page
 
           if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
               <?php
+              if (has_post_thumbnail()) {
+                $featuredImageID = get_post_thumbnail_id();
+                $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+                $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+
+                $featuredImageUrl = $featuredImage[0];
+                $featuredImageAltText = $featuredImageAltText ?: '';
+              } else {
+                $featuredImageUrl = placeHolderImage(600, 800);
+                $featuredImageAltText = 'place holder image';
+              }
               $link = get_field('link');
-              $image = get_field('logo');
               $date = get_field('date');
               ?>
               <div class="relative grid overflow-hidden border rounded-sm border-card-border">
-                <img src="https://images.placeholders.dev/?width=800&height=600&text=FPO" alt="" class="object-cover lg:h-[220px] h-[150px] w-full">
+                <div class="lg:h-[220px] h-[150px] w-full p-sp-4 items-center justify-center grid border-b border-card-border">
+                  <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-container">
+                </div>
                 <div class="grid p-sp-4">
                   <h5 class="mb-sp-4"><?= $date; ?></h5>
                   <h3 class="mb-0"><a href="<?= get_the_ID(); ?>" class="stretched-link"><?= get_the_title(); ?></a></h3>
@@ -72,8 +87,11 @@ Template Name: Press page
           <!-- <div class="relative grid lg:grid-cols-[1fr_4fr] grid-cols-[1fr_2fr] overflow-hidden border rounded-sm border-card-border">
             <img src="https://images.placeholders.dev/?width=800&height=600&text=FPO" alt="" class="object-contain h-[125px] w-full">
             <div class="grid p-sp-4">
-              <h5 class="mb-sp-4"><?php // $date; ?></h5>
-              <h3 class="mb-0"><a href="<?php // get_the_ID(); ?>" class="stretched-link"><?php // get_the_title(); ?></a></h3>
+              <h5 class="mb-sp-4"><?php // $date; 
+                                  ?></h5>
+              <h3 class="mb-0"><a href="<?php // get_the_ID(); 
+                                        ?>" class="stretched-link"><?php // get_the_title(); 
+                                                                    ?></a></h3>
             </div>
           </div> -->
         </div>
