fa366b3e9445dae2cb52f6b6b446a645adaf4461
amacaulay <adam.macaulay@charliehealth.com>
2023-05-31 11:11:44 -0600

    add footer code

---
+++ wp-content/themes/charliehealth/includes/footer-code.php
@@ -0,0 +1,46 @@
+<!-- TODO what is this? -->
+<script>
+  // $(document).ready(function() {
+  //   $("#Phone-Number").mask("(000) 000-0000", {
+  //     placeholder: "(XXX) XXX-XXXX"
+  //   });
+  // });
+</script>
+
+<!-- LinkedIn Insight Tag -->
+<script type="text/javascript">
+  _linkedin_partner_id = "4524642";
+  window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+  window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+</script>
+<script type="text/javascript">
+  (function(l) {
+    if (!l) {
+      window.lintrk = function(a, b) {
+        window.lintrk.q.push([a, b])
+      };
+      window.lintrk.q = []
+    }
+    var s = document.getElementsByTagName("script")[0];
+    var b = document.createElement("script");
+    b.type = "text/javascript";
+    b.async = true;
+    b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+    s.parentNode.insertBefore(b, s);
+  })(window.lintrk);
+</script>
+<noscript>
+  <img height="1" width="1" style="display:none;" alt="Linkedin Pixel" src="https://px.ads.linkedin.com/collect/?pid=4524642&fmt=gif" />
+</noscript>
+<!-- LinkedIn Insight Tag END -->
+
+<!-- Userway -->
+<script>
+  (function() {
+    var s = document.createElement("script");
+    s.setAttribute("data-account", "ltFNShf79S");
+    s.setAttribute("src", "https://cdn.userway.org/widget.js");
+    document.body.appendChild(s);
+  })();
+</script>
+<!-- Userway END -->
