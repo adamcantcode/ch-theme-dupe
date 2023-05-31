da9da755a2553388db527154c93dfab2404325e8
amacaulay <adam.macaulay@charliehealth.com>
2023-05-31 15:05:49 -0600

    fix footer code

--- wp-content/themes/charliehealth/footer.php
+++ wp-content/themes/charliehealth/footer.php
@@ -82,7 +82,7 @@
 </footer>
 <!-- </div> -->
 
-<?php include('wp-content/themes/charliehealth/includes/header-code.php'); ?>
+<?php include('wp-content/themes/charliehealth/includes/footer-code.php'); ?>
 <?php wp_footer(); ?>
 <pre class="fixed left-0 right-0 w-full text-xs text-center text-white bottom-4 -z-10 opacity-30">Made with ❤️ in Denver</pre>
 </body>
