0f79c4427d57c9c8a27c504acc63e3d54478fdc3
amacaulay <adam.macaulay@charliehealth.com>
2023-05-30 21:22:44 -0600

    update template

--- wp-content/themes/charliehealth/page-referral-no-front.php
+++ wp-content/themes/charliehealth/page-referral-no-front.php
@@ -9,42 +9,21 @@ Template Post Type: page
 
 <main id="primary" class="site-main lg:mt-[68px] mt-0">
   <section class="section">
-    <div class="container-md">
-      <div class="text-center">
-        <h1>TEST</h1>
-        <h2><?= get_the_title(); ?></h2>
+    <div class="container">
+      <div class="grid lg:grid-cols-2">
+        <h1><?= get_the_title(); ?></h1>
+        <p>Charlie Health welcomes the opportunity to partner with you to provide excellent care to your patients.</p>
       </div>
-      <h4>Refer a Patient</h4>
-      <p>Charlie Health welcomes the opportunity to partner with you to provide excellent care to your patients.</p>
-      <p>Need help or additional information? Please contact our outreach and admissions teams below for any questions about referring patients to our programs or about the admissions process.</p>
-      <p><i>CH MH Services (CA) LLC is certified by the State Department of Health Care Services (Lic. #300414AP expiring 06/30/2023).</i></p>
-      <p><i>For detailed information on our California Facility Licensure, please visit the <a href="https://geohub-cadhcs.hub.arcgis.com/datasets/CADHCS::sud-recovery-treatment-facilities-1/explore?location=33.650768%2C-117.838283%2C21.00" target="_blank">California Health and Human Services Department's website.</a></i></p>
     </div>
   </section>
   <section class="section-horizontal">
-    <div class="container-md">
+    <div class="container">
       <div class="divider"></div>
     </div>
   </section>
-  <section class="section-top">
-    <div class="container-md">
-      <div class="formmm">
-        [FORM]
-      </div>
-    </div>
-  </section>
   <section class="section">
-    <div class="container-md">
-      <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-sp-16 gap-sp-8">
-        <div>
-          <h4>For general questions about Charlie Health's services and programs, please contact:</h4>
-          <?= $test; ?>
-        </div>
-        <div>
-          <h4>For specific questions about Charlie Health's admissions process, please contact:</h4>
-          <p>Admissions Team (<a href="mailto:admissions@charliehealth.com?subject=Inquiry%20about%20Charlie%20Health%20Admissions">admissions@charliehealth.com</a>)</p>
-        </div>
-      </div>
+    <div class="container">
+      <div><?= get_field('form_code'); ?></div>
     </div>
   </section>
 </main>
