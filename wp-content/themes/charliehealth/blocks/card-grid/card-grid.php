ff70fd0564644789051fd32bedf7d43ee858a1d1
amacaulay <adam.macaulay@charliehealth.com>
2023-06-05 00:23:08 -0600

    typo 🧐

--- wp-content/themes/charliehealth/blocks/card-grid/card-grid.php
+++ wp-content/themes/charliehealth/blocks/card-grid/card-grid.php
@@ -30,7 +30,7 @@ if ($horizontalScroll === true) {
           <div class="w-[calc(100vw-2.5rem)] lg:w-full h-full<?= $link ? ' hover:shadow-lg duration-300 rounded-md' : ''; ?>">
             <div class="<?= $borderStyle === 'gradient' ? 'border-gradient' : ' border rounded-md'; ?> h-full">
               <?php if ($image) : ?>
-                <img src="<?= $image['sizes']['card-thumb'] ?>" alt="<?= $image['aklt']; ?>" class="object-cover w-full rounded-t-md lg:h-[250px] h-[200px]">
+                <img src="<?= $image['sizes']['card-thumb'] ?>" alt="<?= $image['alt']; ?>" class="object-cover w-full rounded-t-md lg:h-[250px] h-[200px]">
               <?php endif; ?>
               <div class="p-sp-4 md:p-sp-6 lg:p-sp-8">
                 <?php if ($numbers) : ?>
