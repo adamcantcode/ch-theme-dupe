93f602ceb8e2a97d932ad166a0947eeccd3572ce
amacaulay <adam.macaulay@charliehealth.com>
2023-06-08 11:45:10 -0600

    padding tweak

--- wp-content/themes/charliehealth/build/blocks/stats-table/stats-table.php
+++ wp-content/themes/charliehealth/build/blocks/stats-table/stats-table.php
@@ -1,56 +1,56 @@
 <div class="grid grid-cols-1 lg:grid-cols-2">
   <div>
     <div class="border-b border-opacity-25 border-card-border">
-      <h2 class="mb-0 text-white p-sp-4">National Averages</h2>
+      <h2 class="mb-0 text-white lg:px-sp-8 py-sp-4">National Averages</h2>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-white aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">80%</h3>
       </div>
       <p class="mb-0 text-white">Of people in the U.S. live in an area without access to appropriate mental healthcare providers*</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-white aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">40%</h3>
       </div>
       <p class="mb-0 text-white">The average % of income families in crisis spend on mental healthcare</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-white aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">8</h3>
       </div>
       <p class="mb-0 text-white">The average number of weeks spent waiting for Intensive Outpatient (IOP) therapy</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-white aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">5x</h3>
       </div>
       <p class="mb-0 text-white">The number of people in need of care vs. available providers</p>
     </div>
   </div>
-  <div class="border-l border-opacity-25 border-card-border">
+  <div class="border-opacity-25 lg:border-l border-card-border">
     <div class="border-b border-opacity-25 border-card-border">
-      <h2 class="mb-0 text-white p-sp-4">Charlie Health</h2>
+      <h2 class="mb-0 text-white lg:px-sp-8 py-sp-4">Charlie Health</h2>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-bright-teal aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">100%</h3>
       </div>
       <p class="mb-0 text-white">Our virtual-first model means we're able to reach people in need who historically haven't had access to treatment</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-bright-teal aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">24/7</h3>
       </div>
       <p class="mb-0 text-white">The numbers of insurers we work with to ensure the lowest cost for treatment</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-bright-teal aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">24/7</h3>
       </div>
       <p class="mb-0 text-white">Get in touch with someone from our team at any time</p>
     </div>
-    <div class="flex items-center gap-sp-4 p-sp-4">
+    <div class="flex items-center gap-sp-4 lg:px-sp-8 py-sp-4">
       <div class="rounded-[50%] bg-bright-teal aspect-square lg:h-sp-16 h-sp-14 p-sp-4 flex items-center justify-center">
         <h3 class="mb-0">100%</h3>
       </div>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '9d0770d9f483a893664d');
+<?php return array('dependencies' => array(), 'version' => 'e03382f964829ee0b9c4');
