514b20d647196197437d5133e4c3815c9e3671ea
amacaulay <adam.macaulay@charliehealth.com>
2023-06-09 10:58:52 -0600

    active tag

--- wp-content/themes/charliehealth/tag.php
+++ wp-content/themes/charliehealth/tag.php
@@ -31,9 +31,9 @@
           'taxonomy' => 'post_tag',
           'hide_empty' => false,
         ));
-        // var_dump($terms);
+        $currentTag = get_queried_object()->slug;
         foreach ($terms as $term) : ?>
-          <a href="<?= $term->slug; ?>" class="no-underline duration-300 rounded-lg px-sp-4 py-sp-3 text-h5 bg-tag-gray hover:bg-bright-teal"><?= $term->name; ?></a>
+          <a href="<?= $term->slug; ?>" class="no-underline duration-300 rounded-lg px-sp-4 py-sp-3 text-h5 <?= $currentTag === $term->slug ? 'bg-dark-blue text-white' : 'bg-tag-gray hover:bg-bright-teal'; ?>"><?= $term->name; ?></a>
         <?php endforeach; ?>
       </div>
     </div>
