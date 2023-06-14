bb42af8c78cbc3999a3bca89c812f02d80a6611e
amacaulay <adam.macaulay@charliehealth.com>
2023-06-14 06:57:47 -0600

    typos

--- wp-content/themes/charliehealth/build/blocks/iop-schedule/iop-schedule.php
+++ wp-content/themes/charliehealth/build/blocks/iop-schedule/iop-schedule.php
@@ -12,21 +12,21 @@
     </div>
     <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/dialectical-behavior-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">DBT Skills Group</a></div>
     <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/yoga-mindfulness-meditation'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Yoga & Mindfulness</a></div>
-    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awarness</a></div>
+    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awareness</a></div>
     <!-- ROW -->
     <div class="border-b p-sp-8 border-card-border">
       <p class="mb-0 border-2 border-transparent py-sp-1">Tuesday</p>
     </div>
     <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">MBT Skills Group</a></div>
-    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/art-and-music-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Art Therpay</a></div>
-    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awarness</a></div>
+    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/art-and-music-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Art Therapay</a></div>
+    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awareness</a></div>
     <!-- ROW -->
     <div class="border-b p-sp-8 border-card-border">
       <p class="mb-0 border-2 border-transparent py-sp-1">Wednesday</p>
     </div>
     <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/cognitive-behavioral-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">CBT Skills Group</a></div>
     <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities/art-and-music-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Music Therapy</a></div>
-    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awarness</a></div>
+    <div class="border-b border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awareness</a></div>
     <!-- ROW -->
     <div class="border-b p-sp-8 border-card-border">
       <p class="mb-0 border-2 border-transparent py-sp-1">Thursday</p>
@@ -40,6 +40,6 @@
     </div>
     <div class="border-l p-sp-8 border-card-border"><a href="<?= site_url('/intensive-outpatient-iop/family-therapy'); ?>" class="w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#5f6387] hover:bg-transparent hover:!text-[#5f6387] hover:border-[#5f6387] rounded-sm block text-center py-sp-1 px-sp-4">Family Session</a></div>
     <div class="border-l p-sp-8 border-card-border"><a href="<?= site_url(''); ?>" class="invisible w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">text text texting</a></div>
-    <div class="border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="invisible w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awarness</a></div>
+    <div class="border-l p-sp-8 border-card-border"><a href="<?= site_url('/treatment-modalities'); ?>" class="invisible w-full text-white no-underline border-2 border-transparent whitespace-nowrap bg-[#35919c] hover:bg-transparent hover:!text-[#35919c] hover:border-[#35919c] rounded-sm block text-center py-sp-1 px-sp-4">Emotional Awareness</a></div>
   </div>
 </div>
