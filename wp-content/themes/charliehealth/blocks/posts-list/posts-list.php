8b15467087c0ac785bbc6ba9580fada8c7edecaa
amacaulay <adam.macaulay@charliehealth.com>
2023-06-16 11:35:24 -0600

    fix thumb

--- wp-content/themes/charliehealth/blocks/posts-list/posts-list.php
+++ wp-content/themes/charliehealth/blocks/posts-list/posts-list.php
@@ -53,39 +53,35 @@ $style  = get_field('style');
   <?php else :
   $customPosts = get_field('custom_posts');
   if ($customPosts) : ?>
-    <section class="section-top">
-      <div class="container">
-        <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10">
-          <?php foreach ($customPosts['related_posts'] as $customPost) : ?>
-            <?php
-            if (has_post_thumbnail($customPost->ID)) {
-              $featuredImageID = get_post_thumbnail_id($customPost->ID);
-              $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
-              $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+    <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10">
+      <?php foreach ($customPosts['related_posts'] as $customPost) : ?>
+        <?php
+        if (has_post_thumbnail($customPost->ID)) {
+          $featuredImageID = get_post_thumbnail_id($customPost->ID);
+          $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+          $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
 
-              $featuredImageUrl = $featuredImage[0];
-              $featuredImageAltText = $featuredImageAltText ?: '';
-            } else {
-              $featuredImageUrl = placeHolderImage(600, 800);
-              $featuredImageAltText = 'place holder image';
-            }
-            ?>
-            <div class="relative grid overflow-hidden duration-300 border rounded-sm border-card-border hover:shadow-lg">
-              <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
-              <div class="grid p-sp-4">
-                <h3><a href="<?= get_the_permalink($customPost->ID); ?>" class="stretched-link"><?= $customPost->post_title; ?></a></h3>
-                <h5 class="mb-sp-4"><?= $author = get_field('by_author')->post_title ?: 'Charlile Health Editorial Team'; ?></h5>
-                <div class="grid items-end justify-start grid-flow-col gap-sp-4">
-                  <?php $tags = get_the_terms($customPost->ID, 'post_tag'); ?>
-                  <?php foreach ($tags as $tag) : ?>
-                    <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
-                  <?php endforeach; ?>
-                </div>
-              </div>
+          $featuredImageUrl = $featuredImage[0];
+          $featuredImageAltText = $featuredImageAltText ?: '';
+        } else {
+          $featuredImageUrl = placeHolderImage(600, 800);
+          $featuredImageAltText = 'place holder image';
+        }
+        ?>
+        <div class="relative grid overflow-hidden duration-300 border rounded-sm border-card-border hover:shadow-lg">
+          <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
+          <div class="grid p-sp-4">
+            <h3><a href="<?= get_the_permalink($customPost->ID); ?>" class="stretched-link"><?= $customPost->post_title; ?></a></h3>
+            <h5 class="mb-sp-4"><?= $author = get_field('by_author')->post_title ?: 'Charlile Health Editorial Team'; ?></h5>
+            <div class="grid items-end justify-start grid-flow-col gap-sp-4">
+              <?php $tags = get_the_terms($customPost->ID, 'post_tag'); ?>
+              <?php foreach ($tags as $tag) : ?>
+                <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
+              <?php endforeach; ?>
             </div>
-          <?php endforeach; ?>
+          </div>
         </div>
-      </div>
-    </section>
+      <?php endforeach; ?>
+    </div>
   <?php endif; ?>
 <?php endif; ?>
