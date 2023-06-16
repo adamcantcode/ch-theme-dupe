a0baf3a7f3c75b2023e10637c236cf18d058dac0
amacaulay <adam.macaulay@charliehealth.com>
2023-06-16 10:51:49 -0600

    custom posts

--- wp-content/themes/charliehealth/build/blocks/posts-list/posts-list.php
+++ wp-content/themes/charliehealth/build/blocks/posts-list/posts-list.php
@@ -1,50 +1,92 @@
 <?php
 $type = get_field('type');
 $count = get_field('count');
+$style  = get_field('style');
 ?>
 
-<div id="<?= $block['id']; ?>">
-  <div class="grid lg:grid-cols-3 gap-x-sp-8 gap-y-sp-10 mb-sp-10">
-    <?php
-    $args = array(
-      'post_type' => $type,
-      'posts_per_page' => $count,
-    );
+<?php if ($style !== 'custom') : ?>
+  <div id="<?= $block['id']; ?>">
+    <div class="grid lg:grid-cols-3 gap-x-sp-8 gap-y-sp-10 mb-sp-10">
+      <?php
+      $args = array(
+        'post_type' => $type,
+        'posts_per_page' => $count,
+      );
 
-    $query = new WP_Query($args);
-    if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-        $author = get_field('by_author', get_the_ID());
-        if (has_post_thumbnail()) {
-          $featuredImageID = get_post_thumbnail_id();
-          $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
-          $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+      $query = new WP_Query($args);
+      if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+          $author = get_field('by_author', get_the_ID());
+          if (has_post_thumbnail()) {
+            $featuredImageID = get_post_thumbnail_id();
+            $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+            $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
 
-          $featuredImageUrl = $featuredImage[0];
-          $featuredImageAltText = $featuredImageAltText ?: '';
-        } else {
-          $featuredImageUrl = placeHolderImage(600, 800);
-          $featuredImageAltText = 'place holder image';
-        }
-    ?>
-        <div class="relative grid overflow-hidden border rounded-sm border-card-border hover:shadow-lg duration-300">
-          <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
-          <div class="grid p-sp-4">
-            <h3><a href="<?= get_the_permalink(); ?>" class="stretched-link"><?= get_the_title(); ?></a></h3>
-            <h5 class="mb-sp-4"><?= $author->post_title; ?></h5>
-            <div class="grid items-end justify-start grid-flow-col gap-sp-4">
-              <?php
-              $tags = get_the_terms(get_the_ID(), 'post_tag');
-              ?>
-              <?php if ($tags) :  ?>
-                <?php foreach ($tags as $tag) : ?>
-                  <a href="<?= get_term_link($tag->slug, 'post_tag'); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
-                <?php endforeach; ?>
-              <?php endif; ?>
+            $featuredImageUrl = $featuredImage[0];
+            $featuredImageAltText = $featuredImageAltText ?: '';
+          } else {
+            $featuredImageUrl = placeHolderImage(600, 800);
+            $featuredImageAltText = 'place holder image';
+          }
+      ?>
+          <div class="relative grid overflow-hidden duration-300 border rounded-sm border-card-border hover:shadow-lg">
+            <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
+            <div class="grid p-sp-4">
+              <h3><a href="<?= get_the_permalink(); ?>" class="stretched-link"><?= get_the_title(); ?></a></h3>
+              <h5 class="mb-sp-4"><?= $author->post_title; ?></h5>
+              <div class="grid items-end justify-start grid-flow-col gap-sp-4">
+                <?php
+                $tags = get_the_terms(get_the_ID(), 'post_tag');
+                ?>
+                <?php if ($tags) :  ?>
+                  <?php foreach ($tags as $tag) : ?>
+                    <a href="<?= get_term_link($tag->slug, 'post_tag'); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
+                  <?php endforeach; ?>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
-        </div>
-    <?php endwhile;
-      wp_reset_postdata();
-    endif; ?>
+      <?php endwhile;
+        wp_reset_postdata();
+      endif; ?>
+    </div>
   </div>
-</div>
+  <?php else :
+  $customPosts = get_field('custom_posts');
+  if ($customPosts) : ?>
+    <section class="section-top">
+      <div class="container">
+        <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10">
+          <?php foreach ($customPosts['related_posts'] as $customPost) : ?>
+            <?php
+            // var_dump($customPost);
+            if (has_post_thumbnail()) {
+              $featuredImageID = get_post_thumbnail_id();
+              $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+              $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+
+              $featuredImageUrl = $featuredImage[0];
+              $featuredImageAltText = $featuredImageAltText ?: '';
+            } else {
+              $featuredImageUrl = placeHolderImage(600, 800);
+              $featuredImageAltText = 'place holder image';
+            }
+            ?>
+            <div class="relative grid overflow-hidden duration-300 border rounded-sm border-card-border hover:shadow-lg">
+              <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
+              <div class="grid p-sp-4">
+                <h3><a href="<?= get_the_permalink($customPost->ID); ?>" class="stretched-link"><?= $customPost->post_title; ?></a></h3>
+                <h5 class="mb-sp-4"><?= $author = get_field('by_author')->post_title ?: 'Charlile Health Editorial Team'; ?></h5>
+                <div class="grid items-end justify-start grid-flow-col gap-sp-4">
+                  <?php $tags = get_the_terms($customPost->ID, 'post_tag'); ?>
+                  <?php foreach ($tags as $tag) : ?>
+                    <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          <?php endforeach; ?>
+        </div>
+      </div>
+    </section>
+  <?php endif; ?>
+<?php endif; ?>
--- wp-content/themes/charliehealth/single.php
+++ wp-content/themes/charliehealth/single.php
@@ -36,7 +36,6 @@ $readingTime = ceil($wordCount / $wordsPerMinute);
 
 <main id="primary" class="site-main mt-[68px]">
   <div id="progressBar" class="fixed z-20 transition-all duration-700 rounded-r-sm bg-purple-gradient-end h-[5px] left-0"></div>
-  <!-- <div class="fixed h-[5px] left-0 w-full bg-card-border z-[15]"></div> -->
   <div id="mainArticleContent" class="relative z-10 main-article-content">
     <section class="section">
       <div class="container">
