872e7b28c097cb008db0de639cb8f9f026e9abf8
amacaulay <adam.macaulay@charliehealth.com>
2023-06-20 22:23:25 -0600

    mobile padding

--- wp-content/themes/charliehealth/build/blocks/cta/cta.php
+++ wp-content/themes/charliehealth/build/blocks/cta/cta.php
@@ -30,7 +30,7 @@ if (!empty($block['backgroundColor'])) {
   </div>
 <?php endif; ?>
 <?php if ($style === 'full') : ?>
-  <div class="grid lg:grid-flow-col justify-between rounded-md p-sp-6 items-center gap-sp-8 <?= $blockClasses; ?>">
+  <div class="grid lg:grid-flow-col justify-between rounded-md lg:p-sp-6 p-sp-4 items-center gap-sp-8 <?= $blockClasses; ?>">
     <div>
       <h2 class="mb-0"><?= $heading; ?></h2>
       <?php if ($subhead) : ?>
