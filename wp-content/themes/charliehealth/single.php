9dad2ba45a27d5b77f88cd37bae12ca61055b509
amacaulay <adam.macaulay@charliehealth.com>
2023-06-28 09:40:55 -0600

    tags conditional

--- wp-content/themes/charliehealth/single.php
+++ wp-content/themes/charliehealth/single.php
@@ -186,9 +186,11 @@ $readingTime = ceil($wordCount / $wordsPerMinute);
               <h5 class="mb-sp-4"><?= $author = get_field('by_author')->post_title ?: 'Charlile Health Editorial Team'; ?></h5>
               <div class="grid items-end justify-start grid-flow-col gap-sp-4">
                 <?php $tags = get_the_terms($post->ID, 'post_tag'); ?>
-                <?php foreach ($tags as $tag) : ?>
-                  <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
-                <?php endforeach; ?>
+                <?php if ($tags) : ?>
+                  <?php foreach ($tags as $tag) : ?>
+                    <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-20 inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
+                  <?php endforeach; ?>
+                <?php endif; ?>
               </div>
             </div>
           </div>
