00c930d458e0a38536bea362fd34394b070a9b33
amacaulay <adam.macaulay@charliehealth.com>
2023-07-18 14:50:53 -0600

    dynamic, add colors

--- wp-content/themes/charliehealth/build/blocks/testimonial-review/testimonial-review.php
+++ wp-content/themes/charliehealth/build/blocks/testimonial-review/testimonial-review.php
@@ -7,84 +7,60 @@
     </div>
     <div class="masonry-js">
       <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full grid-sizer"></div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do before Charlie Health.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“They made it a safe and comfortable environment...I can and will survive, even through the toughest of times.”</h3>
-        <p class="leading-snug mb-sp-8">I have struggled with mental health for as long as I remember. I was sent impatient a bunch of times but nothing was really helping me. I went to an RTC (residential treatment center) and then went straight to Charlie Health. Charlie Health has taught me so much about how there are a lot of good people out there who truly care about how you're doing. I learned a lot about how to interact and socialize better with people (I am on the spectrum, so that part is a little bit harder for me). Charlie Health taught me new skills and gave me feedback that I can use in everyday life. I overall have loved being in Charlie Health because they made it a safe and comfortable environment. Charlie Health has definitely helped me in the long run, and has taught me that I can and will survive, even through the toughest of times.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do before Charlie Health.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do before Charlie Health.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“They made it a safe and comfortable environment...I can and will survive, even through the toughest of times.”</h3>
-        <p class="leading-snug mb-sp-8">I have struggled with mental health for as long as I remember. I was sent impatient a bunch of times but nothing was really helping me. I went to an RTC (residential treatment center) and then went straight to Charlie Health. Charlie Health has taught me so much about how there are a lot of good people out there who truly care about how you're doing. I learned a lot about how to interact and socialize better with people (I am on the spectrum, so that part is a little bit harder for me). Charlie Health taught me new skills and gave me feedback that I can use in everyday life. I overall have loved being in Charlie Health because they made it a safe and comfortable environment. Charlie Health has definitely helped me in the long run, and has taught me that I can and will survive, even through the toughest of times.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“I really didn’t know what to do before Charlie Health.”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
-      <div class="lg:w-[calc(50%-16px)] noshow opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col">
-        <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-purple-gradient-end mb-sp-8">13 year old</span>
-        <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“TEST”</h3>
-        <p class="leading-snug mb-sp-8">I’ve always felt I've been alone in this. I felt so helpless. Within the first week [of her starting Charlie Health], my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’ve been blown away. I’m very impressed and very happy.</p>
-        <p class="mb-0">—Tasia C.</p>
-      </div>
+      <?php
+      $args = array(
+        'numberposts' => -1,
+        'posts_per_page' => -1,
+        'post_type' => 'testimonial',
+      );
+      $query = new WP_Query($args);
+      ?>
+      <?php if ($query->have_posts()) : ?>
+        <?php
+        $count = 0;
+        ?>
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
+          <?php
+          $count++;
+          $attribution = get_field('attribution', get_the_ID());
+          $pullQuote = get_field('pull-quote', get_the_ID());
+          $fullQuote = get_field('full_quote', get_the_ID());
+          $age = get_field('age', get_the_ID());
+          $group = get_the_terms(get_the_ID(), 'testimonials-group')[0]->slug;
+
+          switch ($group) {
+            case 'young-adult':
+              $tagBGColor = 'bg-young-adult';
+              break;
+            case 'teen':
+              $tagBGColor = 'bg-teen';
+              break;
+            case 'parent':
+              $tagBGColor = 'bg-parent';
+              break;
+            default:
+              $tagBGColor = '';
+              break;
+          }
+
+          ?>
+          <div class="lg:w-[calc(50%-16px)] opacity-0 scale-95 w-full mb-sp-8 rounded-[1rem] p-sp-8 testimonial-item bg-white flex flex-col<?= $count > 6 ? ' noshow' : ''; ?>">
+            <?php if ($tagBGColor && $age) : ?>
+              <?php if ($group !== 'parent') : ?>
+                <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 mb-sp-8 php <?= $tagBGColor; ?> "><?= $age; ?> year old</span>
+                <?php else : ?>
+                  <span class="relative z-20 self-start no-underline rounded-lg px-sp-4 py-sp-3 text-h6 mb-sp-8 php <?= $tagBGColor; ?> ">Parent of a <?= $age; ?>-year-old</span>
+              <?php endif; ?>
+            <?php endif; ?>
+            <?php if ($pullQuote) : ?>
+              <h3 class="leading-tight mb-sp-2 lg:text-[2rem]">“<?= $pullQuote; ?>”</h3>
+            <?php endif; ?>
+            <p class="leading-snug mb-sp-8"><?= $fullQuote; ?></p>
+            <p class="mb-0">—<?= $attribution; ?></p>
+          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+      <?php wp_reset_query(); ?>
     </div>
     <div class="grid lg:col-start-2">
       <a role="button" class="ch-button button-secondary justify-self-center load-more-js">Load more</a>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '78f4a85824a24d411e7c');
+<?php return array('dependencies' => array(), 'version' => '19a835e78d23e8413c58');
