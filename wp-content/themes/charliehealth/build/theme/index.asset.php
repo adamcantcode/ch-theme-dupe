5cd98e942b2d6281698aa0044a356e63f1676957
amacaulay <adam.macaulay@charliehealth.com>
2023-08-21 15:37:37 -0600

    start form slides

--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '420fc46af6f4267f0eb5');
+<?php return array('dependencies' => array(), 'version' => 'bab38a5c67b67e1c4b33');
---
+++ wp-content/themes/charliehealth/page-form.php
@@ -0,0 +1,40 @@
+<?php
+/*
+Template Name: Form Page
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<main id="primary" class="site-main mt-[68px]">
+  <div class="section">
+    <div class="container">
+      <div class="grid grid-cols-1 lg:grid-cols-2">
+        <div>
+          <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/test_charlie_health_webform_copy_1"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/test_charlie_health_webform_copy_1" title="Online Form">Online Form - [LIVE] Charlie Health Webform 2.0</a></noscript>
+          <div style="text-align:right; font-size:x-small;">
+          </div>
+        </div>
+        <div class="relative">
+          <div class="absolute mx-sp-16 mt-sp-16">
+            <div class="rounded-md p-sp-8 bg-purple-gradient-end">
+              <h3>This program saved my lufe</h3>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum ipsam dignissimos dolor quibusdam veniam eligendi aliquam amet cupiditate! Sint corrupti enim assumenda corporis, iste magnam quisquam possimus, provident, dolor dolorum doloribus! Itaque alias eum iste fugiat dicta perspiciatis debitis ratione, fugit totam amet! Fugiat, blanditiis!</p>
+              <p>--Elora</p>
+            </div>
+          </div>
+          <div class="absolute mx-sp-16 mt-sp-16">
+            <div class="rounded-md p-sp-8 bg-purple-gradient-end">
+              <h3>This program saved my lufe</h3>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum ipsam dignissimos dolor quibusdam veniam eligendi aliquam amet cupiditate! Sint corrupti enim assumenda corporis, iste magnam quisquam possimus, provident, dolor dolorum doloribus! Itaque alias eum iste fugiat dicta perspiciatis debitis ratione, fugit totam amet! Fugiat, blanditiis!</p>
+              <p>--Elora</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php get_footer(); ?>
