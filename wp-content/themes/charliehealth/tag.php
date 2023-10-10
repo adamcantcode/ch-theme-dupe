a6a2dafeaec6ea6bf2976bbcf5afb7dac43ea771
amacaulay <adam.macaulay@charliehealth.com>
2023-10-10 13:32:18 -0400

    add research posts

--- wp-content/themes/charliehealth/tag.php
+++ wp-content/themes/charliehealth/tag.php
@@ -268,8 +268,43 @@
       endif; ?>
     </section>
     <section class="section" id="section5">
-      <div class="container-sm">
-        <?= $sectionFive; ?>
+      <div class="container">
+        <?php $relatedResearchPost = get_field('related_research_post', get_queried_object()); ?>
+        <?php if ($relatedResearchPost) : ?>
+          <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-sp-16 gap-sp-8">
+          <?php endif; ?>
+          <div>
+            <?= $sectionFive; ?>
+          </div>
+          <?php if ($relatedResearchPost) : ?>
+            <?php
+            if (has_post_thumbnail($relatedResearchPost->ID)) {
+              $featuredImageID = get_post_thumbnail_id($relatedResearchPost->ID);
+              $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+              $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+
+              $featuredImageUrl = $featuredImage[0];
+              $featuredImageAltText = $featuredImageAltText ?: '';
+            } else {
+              $featuredImageUrl = site_url('/wp-content/uploads/2023/06/charlie-health_find-your-group.png.webp');
+              $featuredImageAltText = 'Charlie Health Logo';
+            }
+            ?>
+            <div class="relative grid overflow-hidden duration-300 border rounded-sm border-card-border hover:shadow-lg">
+              <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover lg:h-[220px] h-[150px] w-full">
+              <div class="grid p-sp-4">
+                <h3><a href="<?= get_the_permalink($relatedResearchPost->ID); ?>" class="stretched-link"><?= $relatedResearchPost->post_title; ?></a></h3>
+                <h5 class="mb-sp-4"><?= $author = get_field('by_author')->post_title ?: 'Charlile Health Editorial Team'; ?></h5>
+                <div class="grid items-end justify-start grid-flow-col gap-sp-4">
+                  <?php $tags = get_the_terms($relatedResearchPost->ID, 'post_tag'); ?>
+                  <?php foreach ($tags as $tag) : ?>
+                    <a href="<?= get_tag_link($tag->term_id); ?>" class="relative z-[6] inline-block no-underline rounded-lg px-sp-4 py-sp-3 text-h6 bg-tag-gray hover:bg-bright-teal"><?= $tag->name; ?></a>
+                  <?php endforeach; ?>
+                </div>
+              </div>
+            </div>
+          </div>
+        <?php endif; ?>
       </div>
     </section>
   </main>
