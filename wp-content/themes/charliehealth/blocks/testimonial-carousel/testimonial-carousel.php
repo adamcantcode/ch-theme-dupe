90f083c324b924de27492116de9c5deeae0f825d
amacaulay <adam.macaulay@charliehealth.com>
2023-10-11 17:47:32 -0400

    arrow margin

--- wp-content/themes/charliehealth/blocks/testimonial-carousel/testimonial-carousel.php
+++ wp-content/themes/charliehealth/blocks/testimonial-carousel/testimonial-carousel.php
@@ -4,61 +4,40 @@
       <path d="M0.820824 2.83575C0.820823 1.29615 2.48749 0.333897 3.82082 1.1037L52.9361 29.4604C54.2694 30.2302 54.2694 32.1547 52.9361 32.9245L3.82083 61.2812C2.48749 62.051 0.820824 61.0888 0.820824 59.5492L0.820824 2.83575Z" fill="#F7F5F1" />
     </svg>
   </div>
-  <div class="swiper swiper-testimonial-carousel-big">
-    <div class="swiper-wrapper">
-      <div class="!h-auto swiper-slide">
-        <div class="grid lg:grid-cols-2">
-          <div class="order-2 lg:order-1">
-            <h5 class="lg:mt-sp-14 mb-sp-8">In their words</h5>
-            <div class="text-[56px] text-primary leading-[1.1] font-heading-serif mb-sp-8">“Everyone thinks something is wrong with me.”</div>
-            <p>Mental health stigma is more than calling those with mental health issues “crazy” — it’s an entrenched societal belief that people struggling with their mental health are abnormal, dangerous, or permanently damaged. </p>
-            <p class="lg:mb-0 mb-sp-14">- Anonymous, Teen</p>
+  <?php if (have_rows('testimonials')) : ?>
+    <div class="swiper swiper-testimonial-carousel-big">
+      <div class="swiper-wrapper">
+        <?php while (have_rows('testimonials')) : the_row(); ?>
+          <?php
+          $testimonial = get_sub_field('testimonial');
+          $postID = $testimonial->ID;
+
+          $anonymous = get_field('anonymous', $postID);
+          if ($anonymous === false) {
+            $attribution = get_field('attribution', $postID);
+          } else {
+            $attribution = 'Anonymous';
+          }
+          $pullQuote = get_field('pull-quote', $postID);
+          $fullQuote = get_field('full_quote', $postID);
+          $illo = get_sub_field('illustration');
+          ?>
+          <div class="!h-auto swiper-slide">
+            <div class="grid lg:grid-cols-2 mb-sp-16">
+              <div class="order-2 lg:order-1">
+                <h5 class="lg:mt-sp-14 mb-sp-8">In their words</h5>
+                <div class="text-[56px] text-primary leading-[1.1] font-heading-serif mb-sp-8">“<?= $pullQuote; ?>”</div>
+                <p><?= $fullQuote; ?></p>
+                <p class="lg:mb-0 mb-sp-14">— <?= $attribution; ?></p>
+              </div>
+              <div class="order-1 lg:order-2">
+                <img src="<?= $illo['url']; ?>" alt="<?= $illo['alt']; ?>" class="mt-sp-14 lg:mt-0" data-swiper-parallax-scale="0.98">
+              </div>
+            </div>
           </div>
-          <div class="order-1 lg:order-2">
-            <img src="https://www.charliehealth.com/wp-content/uploads/2023/09/X-1.png.webp" alt="test" class="grid lg:grid-cols-2 mt-sp-14 lg:mt-0" data-swiper-parallax-scale="0.98">
-          </div>
-        </div>
+        <?php endwhile; ?>
       </div>
-      <div class="!h-auto swiper-slide">
-        <div class="grid lg:grid-cols-2">
-          <div class="order-2 lg:order-1">
-            <h5 class="lg:mt-sp-14 mb-sp-8">In their words</h5>
-            <div class="text-[56px] text-primary leading-[1.1]  font-heading-serif mb-sp-8">“I've dealt with severe anxiety my whole life.”</div>
-            <p>I developed agoraphobia, and I was depressed that I was missing out on everyday teen things. But when I started going to groups at Charlie Health, i gained the confidence to begin my recovery journey. I now have the resources and skills I need to leave my house</p>
-            <p class="lg:mb-0 mb-sp-14">- Anonymous, Teen</p>
-          </div>
-          <div class="order-1 lg:order-2">
-            <img src="https://www.charliehealth.com/wp-content/uploads/2023/09/X-4.png.webp" alt="test" class="grid lg:grid-cols-2 mt-sp-14 lg:mt-0" data-swiper-parallax-scale="0.98">
-          </div>
-        </div>
-      </div>
-      <div class="!h-auto swiper-slide">
-        <div class="grid lg:grid-cols-2">
-          <div class="order-2 lg:order-1">
-            <h5 class="lg:mt-sp-14 mb-sp-8 text-[14px]">In their words</h5>
-            <div class="text-[56px] text-primary leading-[1.1]  font-heading-serif mb-sp-8">“Everyone thinks something is wrong with me.”</div>
-            <p>Mental health stigma is more than calling those with mental health issues “crazy” — it’s an entrenched societal belief that people struggling with their mental health are abnormal, dangerous, or permanently damaged. </p>
-            <p class="lg:mb-0 mb-sp-14">- Anonymous, Teen</p>
-          </div>
-          <div class="order-1 lg:order-2">
-            <img src="https://placehold.co/657x657" alt="test" class="grid lg:grid-cols-2 mt-sp-14 lg:mt-0" data-swiper-parallax-scale="0.98 mt-sp-14 lg:mt-0">
-          </div>
-        </div>
-      </div>
-      <div class="!h-auto swiper-slide">
-        <div class="grid lg:grid-cols-2">
-          <div class="order-2 lg:order-1">
-            <h5 class="lg:mt-sp-14 mb-sp-8">In their words</h5>
-            <div class="text-[56px] text-primary leading-[1.1]  font-heading-serif mb-sp-8">“Everyone thinks something is wrong with me.”</div>
-            <p>Mental health stigma is more than calling those with mental health issues “crazy” — it’s an entrenched societal belief that people struggling with their mental health are abnormal, dangerous, or permanently damaged. </p>
-            <p class="lg:mb-0 mb-sp-14">- Anonymous, Teen</p>
-          </div>
-          <div class="order-1 lg:order-2">
-            <img src="https://placehold.co/657x657" alt="test" class="grid lg:grid-cols-2 mt-sp-14 lg:mt-0" data-swiper-parallax-scale="0.98 mt-sp-14 lg:mt-0">
-          </div>
-        </div>
-      </div>
+      <div class="swiper-pagination"></div>
     </div>
-    <div class="swiper-pagination"></div>
-  </div>
+  <?php endif; ?>
 </div>
