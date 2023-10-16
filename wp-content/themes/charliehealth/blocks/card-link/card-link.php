716d1053120b7933e74d8c48249e05ec5459290f
amacaulay <adam.macaulay@charliehealth.com>
2023-10-16 11:21:21 -0400

    add card link block

---
+++ wp-content/themes/charliehealth/blocks/card-link/card-link.php
@@ -0,0 +1,7 @@
+<div class="rounded-sm p-sp-4 grid grid-cols-[1fr_2fr] gap-sp-6 shadow-[3px_3px_0px_0px_rgba(255,255,255,1)]">
+  <img src="https://placehold.co/85x110/" alt="">
+  <div>
+    <h4>Explore the data -></h4>
+    <h5>Our Annual Outcomes Report</h5>
+  </div>
+</div>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '83f6085bacef84bdc318');
+<?php return array('dependencies' => array(), 'version' => '054993053474e4b18eac');
