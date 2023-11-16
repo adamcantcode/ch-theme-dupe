46f44688a98a3bdbb24cee3f8db70fdb08dcb103
amacaulay <adam.macaulay@charliehealth.com>
2023-11-16 10:08:46 -0500

    footer desktop

--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '60ad12cb0bf898a17ab7');
+<?php return array('dependencies' => array(), 'version' => '75b8c60e30ec37ec5309');
--- wp-content/themes/charliehealth/footer.php
+++ wp-content/themes/charliehealth/footer.php
@@ -3,7 +3,7 @@
 		<div class="container ">
 			<div class="flex flex-col justify-between lg:flex-row">
 				<div class="w-full rounded-t-sm lg:rounded-l-sm lg:rounded-r-none py-sp-5 px-sp-6 bg-lavender-100">
-					<p class="mb-0">If this is a life-threatening emergency, please call 911 or the <a href="https://988lifeline.org/" target="_blank" class="text-darker-blue">National Suicide Prevention Lifeline</a></p>
+					<p class="mb-0 text-[14px] leading-none">If this is a life-threatening emergency, please call 911 or the <a href="https://988lifeline.org/" target="_blank" class="text-darker-blue">National Suicide Prevention Lifeline</a></p>
 				</div>
 				<div class="rounded-b-sm bg-lavender-300 lg:rounded-r-sm lg:rounded-l-none py-sp-5 px-sp-6">
 					<a href="tel:+988" class="flex items-center no-underline gap-sp-2 text-darker-blue">
@@ -14,21 +14,21 @@
 			</div>
 		</div>
 		<div class="container">
-			<div class="grid grid-cols-2 lg:grid-cols-4 py-sp-12">
+			<div class="grid grid-flow-col grid-cols-2 lg:grid-cols-6 py-sp-12">
 				<?php
 				if (have_rows('footer_navigation_item_new', 'option')) :
 					while (have_rows('footer_navigation_item_new', 'option')) : the_row();
 						$heading = get_sub_field('footer_column_heading')
 				?>
 						<div class="flex flex-col lg:mb-0 mb-sp-6">
-							<p class="font-heading text-nav-normal mb-sp-3 last:mb-0 text-lavender-200"><?= $heading; ?></p>
+							<p class="font-heading text-nav-normal mb-sp-4 last:mb-0 text-lavender-200"><?= $heading; ?></p>
 							<?php
 							if (have_rows('footer_submenu_items', 'option')) :
 								while (have_rows('footer_submenu_items', 'option')) : the_row();
 									$sublinkUrl = get_sub_field('footer_sublink')['url'];
 									$sublinkTitle = get_sub_field('footer_sublink')['title'];
 							?>
-									<a href="<?= $sublinkUrl; ?>" class="text-white no-underline font-heading text-nav-normal mb-sp-3 last:mb-0 hover:text-lavender-200"><?= $sublinkTitle; ?></a>
+									<a href="<?= $sublinkUrl; ?>" class="text-white no-underline font-heading text-nav-normal mb-[14px] last:mb-0 hover:text-lavender-200"><?= $sublinkTitle; ?></a>
 								<?php endwhile; ?>
 							<?php endif; ?>
 						</div>
@@ -49,10 +49,10 @@
 						}
 					}
 					?>
-					<?php if (!$removeNewsletter) : ?>
+					<?php if (!0 == true) : ?>
 						<div class="flex-grow">
-							<h3 class="text-lavender-200 font-heading mb-sp-2 text-[28px]">Stay connected</h3>
-							<p class="text-white">Get mental health updates, research, insights, and resources directly to your inbox.</p>
+							<h3 class="text-white font-heading mb-sp-2 text-[28px]">Stay connected</h3>
+							<p class="text-white text-[14px] leading-[130%]">Get mental health updates, research, insights, and resources directly to your inbox.</p>
 							<div id="newsletterFooter" class="w-full lg:w-1/2 newsletter-revamp footer-newsletter">
 								<script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/newsletter_blog_revamp"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/newsletter_blog_revamp" title="Online Form">Online Form - Newsletter - Blog Revamp</a></noscript>
 								<script>
@@ -81,21 +81,21 @@
 						</div>
 					<?php endif; ?>
 					<div>
-						<div class="flex gap-sp-4">
+						<div class="flex gap-sp-1">
 							<a href="https://www.facebook.com/charliehealth" target="_blank">
-								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/facebook.svg'); ?>" alt="Facebook logo" class="w-[42px] h-[42px] p-sp-2 filter-white" />
+								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/facebook.svg'); ?>" alt="Facebook logo" class="w-[42px] h-[42px] p-[10px] filter-white" />
 							</a>
 							<a href="https://www.linkedin.com/company/charlie-health/" target="_blank">
-								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/linkedin.svg'); ?>" alt="LinkedIn logo" class="w-[42px] h-[42px] p-sp-2 filter-white" />
+								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/linkedin.svg'); ?>" alt="LinkedIn logo" class="w-[42px] h-[42px] p-[10px] filter-white" />
 							</a>
 							<a href="https://twitter.com/charliehealth" target="_blank">
-								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/x.svg'); ?>" alt="Twitter (X) logo" class="w-[42px] h-[42px] p-sp-2 filter-white" />
+								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/x.svg'); ?>" alt="Twitter (X) logo" class="w-[42px] h-[42px] p-[10px] filter-white" />
 							</a>
 							<a href="https://www.instagram.com/charliehealth/" target="_blank">
-								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/instagram.svg'); ?>" alt="Instagram logo" class="w-[42px] h-[42px] p-sp-2 filter-white" />
+								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/instagram.svg'); ?>" alt="Instagram logo" class="w-[42px] h-[42px] p-[10px] filter-white" />
 							</a>
 							<a href="https://www.tiktok.com/@charliehealth" target="_blank">
-								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/tik-tok.svg'); ?>" alt="TikTok logo" class="w-[42px] h-[42px] p-sp-2 filter-white" />
+								<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/social-logos/tik-tok.svg'); ?>" alt="TikTok logo" class="w-[42px] h-[42px] p-[10px] filter-white" />
 							</a>
 						</div>
 					</div>
@@ -118,16 +118,16 @@
 			</div>
 		</div>
 		<div class="container mb-sp-8">
-			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[18px]">All professional medical services are provided by licensed physicians and clinicians affiliated with independently owned and operated professional practices. For patients in California, this is known as “CH Medical CA, P.C.” For patients in North Carolina or New Jersey, this is known as “CH Medical NC NJ, P.C.” For patients in New York, this is known as “CH Medical NY”. For patients in all other states, this is known as “Charlie Health Medical, P.A.” Charlie Health, Inc. provides administrative and technology services to the CH Medical practices it supports, and does not provide any professional medical services itself.</p>
-			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[18px]">If you are located in California, the below applies to you, subject to applicable law:</p>
-			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[18px]"><em>Your health plan’s contracted network providers may also offer in-office appointments. Health plan’s telehealth and in-person services are subject to the same timely access to care standards. If you have out-of-network benefits, and utilize out-of-network services, you are subject to the plan’s cost-sharing obligation and balance billing protections. Contact your health plan to learn more.</em></p>
-			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[18px]"><em>You have a right to access your medical records. Records of the care you receive from Charlie Health will be shared with your primary care provider (PCP) via an electronic patient record system or provided in a different manner unless you opt out.</em></p>
+			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[12px]">All professional medical services are provided by licensed physicians and clinicians affiliated with independently owned and operated professional practices. For patients in California, this is known as “CH Medical CA, P.C.” For patients in North Carolina or New Jersey, this is known as “CH Medical NC NJ, P.C.” For patients in New York, this is known as “CH Medical NY”. For patients in all other states, this is known as “Charlie Health Medical, P.A.” Charlie Health, Inc. provides administrative and technology services to the CH Medical practices it supports, and does not provide any professional medical services itself.</p>
+			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[12px]">If you are located in California, the below applies to you, subject to applicable law:</p>
+			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[12px]"><em>Your health plan’s contracted network providers may also offer in-office appointments. Health plan’s telehealth and in-person services are subject to the same timely access to care standards. If you have out-of-network benefits, and utilize out-of-network services, you are subject to the plan’s cost-sharing obligation and balance billing protections. Contact your health plan to learn more.</em></p>
+			<p class="text-[#87889A] text-[12px] leading-snug mb-sp-[12px]"><em>You have a right to access your medical records. Records of the care you receive from Charlie Health will be shared with your primary care provider (PCP) via an electronic patient record system or provided in a different manner unless you opt out.</em></p>
 		</div>
 		<div class="container">
 			<div class="grid lg:grid-cols-2 grid-cols-[2fr_1fr]">
 				<div class="flex items-center">
 					<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield.svg'); ?>" alt="Charlie health shield logo" class="w-sp-8" />
-					<span class="text-white text-h5 ml-sp-4">© <?= date('Y'); ?> Charlie Health, Inc. All rights reserved.</span>
+					<span class="text-white text-[12px] ml-sp-4">© <?= date('Y'); ?> Charlie Health, Inc. All rights reserved.</span>
 				</div>
 				<a href="https://www.jointcommission.org/" target="_blank">
 					<img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/gold-seal.webp'); ?>" alt="The Joint Commission logo that links to the Joint Commission homepage" class="ml-auto w-[56px]">
