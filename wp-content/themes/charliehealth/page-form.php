02725e668163ddb4c004c7e36d3047be5418fe72
amacaulay <adam.macaulay@charliehealth.com>
2023-11-20 13:36:04 -0700

    wpe source

--- wp-content/themes/charliehealth/page-form.php
+++ wp-content/themes/charliehealth/page-form.php
@@ -11,37 +11,27 @@ Template Post Type: page
 /**
  * Gets IP address.
  */
-function getIpAddress()
+function get_the_user_ip()
 {
-  $ipAddress = '';
+
   if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    // to get shared ISP IP address
-    $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
-  } else if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    // check for IPs passing through proxy servers
-    // check if multiple IP addresses are set and take the first one
-    $ipAddressList = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-    foreach ($ipAddressList as $ip) {
-      if (!empty($ip)) {
-        // if you prefer, you can check for valid IP address here
-        $ipAddress = $ip;
-        break;
-      }
-    }
-  } else if (!empty($_SERVER['HTTP_X_FORWARDED'])) {
-    $ipAddress = $_SERVER['HTTP_X_FORWARDED'];
-  } else if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
-    $ipAddress = $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-  } else if (!empty($_SERVER['HTTP_FORWARDED_FOR'])) {
-    $ipAddress = $_SERVER['HTTP_FORWARDED_FOR'];
-  } else if (!empty($_SERVER['HTTP_FORWARDED'])) {
-    $ipAddress = $_SERVER['HTTP_FORWARDED'];
-  } else if (!empty($_SERVER['REMOTE_ADDR'])) {
-    $ipAddress = $_SERVER['REMOTE_ADDR'];
+
+    //check ip from share internet
+
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+
+    //to check ip is pass from proxy
+
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+
+    $ip = $_SERVER['REMOTE_ADDR'];
   }
-  return $ipAddress;
+
+  return apply_filters('wpb_get_ip', $ip);
 }
-$ipAddress = getIpAddress();
+$ipAddress = get_the_user_ip();
 ?>
 <div id="userIP" class="noshow"><?= $ipAddress; ?></div>
 
