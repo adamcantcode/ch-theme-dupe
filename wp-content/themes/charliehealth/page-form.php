ad55c5fc9e439b1a2b7159defaef3fb5833f2db9
amacaulay <adam.macaulay@charliehealth.com>
2023-11-20 12:21:07 -0700

    just remote IP

--- wp-content/themes/charliehealth/page-form.php
+++ wp-content/themes/charliehealth/page-form.php
@@ -8,23 +8,7 @@ Template Post Type: page
 <?php get_header(); ?>
 
 <?php
-function getIPAddress()
-{
-  //whether ip is from the share internet  
-  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-  }
-  //whether ip is from the proxy  
-  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  }
-  //whether ip is from the remote address  
-  else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-  }
-  return $ip;
-}
-$ip = getIPAddress();
+$ip = $_SERVER['REMOTE_ADDR'];
 ?>
 <div id="userIP" class="noshow"><?= $ip; ?></div>
 
