b030cd913f71a223adae8b287f2ee9f43417a1a1
amacaulay <adam.macaulay@charliehealth.com>
2023-12-07 12:11:49 -0500

    spacing

--- wp-content/themes/charliehealth/blocks/aoc-list/aoc-list.php
+++ wp-content/themes/charliehealth/blocks/aoc-list/aoc-list.php
@@ -1,5 +1,5 @@
 <div class="grid lg:grid-cols-[3fr_1fr_8fr] gap-x-sp-5">
-  <div>
+  <div class="mb-sp-1">
     <h2>What do we treat in our virtual IOP?</h2>
   </div>
   <div></div>
@@ -18,7 +18,7 @@
       if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
           <div class="relative">
             <a href="<?= get_the_permalink(); ?>" class="stretched-link no-underline pb-sp-6 border-b-2 border-primary grid lg:grid-cols-[3fr_4.4fr] mb-sp-6 gap-x-sp-5 items-center list-item-height-js group">
-              <div class="mb-sp-4 lg:mb-0">
+              <div class="mb-sp-2 lg:mb-0">
                 <h4 class="inline-block mb-0"><?= ucfirst(strtolower(get_the_title())); ?></h4>
                 <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="group-hover:translate-x-[5px] transition-all duration-300 ml-sp-4 inline-block align-baseline">
                   <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="#161A3D" />
