c2abfd88c8f3a37b9ca00d693c7d0ab55d116eb3
amacaulay <adam.macaulay@charliehealth.com>
2023-12-14 15:49:13 -0500

    photo on form

--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'ca90609ffa40f1777e8b');
+<?php return array('dependencies' => array(), 'version' => 'a587c9a60e1f19706fb3');
--- wp-content/themes/charliehealth/page-form.php
+++ wp-content/themes/charliehealth/page-form.php
@@ -9,7 +9,7 @@ Template Post Type: page
 
 <div class="section">
   <div class="container">
-    <div class="grid grid-cols-1 lg:grid-cols-2">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-sp-8">
       <div>
         <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/test_charlie_health_webform_copy_1"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/test_charlie_health_webform_copy_1" title="Online Form">Online Form - [LIVE] Charlie Health Webform 2.0</a></noscript>
         <script>
@@ -102,40 +102,6 @@ Template Post Type: page
         </div>
       </div>
       <div class="grid items-center">
-        <div class="grid-cols-1 duration-500 opacity-100 lg:mx-sp-16 lg:mt-0 mt-sp-4 quote" style="grid-area: 1/1;">
-          <div class="rounded-md p-sp-8 bg-young-adult">
-            <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health shield logo" class="ml-auto w-sp-10 mb-sp-8">
-            <h3 class="lg:text-[41px] text-h2 mb-sp-3 leading-tight">“My daughter was acting like herself again.”</h3>
-            <p>I really didn’t know what to do for my daughter before Charlie Health. I’ve always felt I've been alone in this. I felt so helpless. Within her first week, my daughter was acting like herself again. Charlie Health has given my daughter and me lifelong tools to navigate her anxiety and panic attacks. I’m very impressed and very happy.</p>
-            <p class="mb-0">—Tasia, parent of 17-year-old</p>
-          </div>
-        </div>
-        <div class="grid-cols-1 duration-500 opacity-0 lg:mx-sp-16 lg:mt-0 mt-sp-4 quote" style="grid-area: 1/1;">
-          <div class="rounded-md p-sp-8 bg-parent">
-            <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health shield logo" class="ml-auto w-sp-10 mb-sp-8">
-            <h3 class="lg:text-[41px] text-h2 mb-sp-3 leading-tight">“So supportive.”</h3>
-            <p>I was unsure about going into something virtual, but it was so helpful! I think being in my own space while talking about difficult things added to the safety I felt. The facilitators and staff really truly care about the well-being of group members and are so supportive.</p>
-            <p class="mb-0">—Anonymous, 21, young adult</p>
-          </div>
-        </div>
-        <div class="grid-cols-1 duration-500 opacity-0 lg:mx-sp-16 lg:mt-0 mt-sp-4 quote" style="grid-area: 1/1;">
-          <div class="rounded-md p-sp-8 bg-[#E07058]">
-            <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health shield logo" class="ml-auto w-sp-10 mb-sp-8">
-            <h3 class="lg:text-[41px] text-h2 mb-sp-3 leading-tight">“This program saved my life.”</h3>
-            <p>Charlie Health has been a very helpful and supportive environment. I really like how much easier it is to get mental healthcare virtually, and the way they tailor your individual experience to your needs and your lived experiences. I would definitely recommend the program to others. It has helped me tremendously as I heal.</p>
-            <p class="mb-0">—Andrew, 25, young adult</p>
-          </div>
-        </div>
-        <div class="grid-cols-1 duration-500 opacity-0 lg:mx-sp-16 lg:mt-0 mt-sp-4 quote" style="grid-area: 1/1;">
-          <div class="rounded-md p-sp-8 bg-teen">
-            <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health shield logo" class="ml-auto w-sp-10 mb-sp-8">
-            <h3 class="lg:text-[41px] text-h2 mb-sp-3 leading-tight">“This program saved my life.”</h3>
-            <p>When I got to Charlie Health, I did not want to accept any more help. But after my first group session, I was smiling again for the first time in 7 months. I connected and shared with people in ways I had been very closed off to. I loved my time here, and I looked forward to group every day. I love Charlie Health and thank them for getting me out of place I never thought I could.</p>
-            <p class="mb-0">—Nia, 15, teen</p>
-          </div>
-        </div>
-      </div>
-      <div class="grid items-center noshow">
         <img src="https://www.charliehealth.com/wp-content/uploads/2023/11/Device_Laptop.png.webp" alt="Illustration of Charlie Health client using laptop for Virtual IOP Therapy">
       </div>
     </div>
