6106d76763abe61419f08c0c4e82b3a65dd92bdd
amacaulay <adam.macaulay@charliehealth.com>
2023-12-22 16:06:41 -0500

    size

--- wp-content/themes/charliehealth/build/blocks/image-grid/image-grid.php
+++ wp-content/themes/charliehealth/build/blocks/image-grid/image-grid.php
@@ -1,7 +1,7 @@
-<div class="grid justify-around grid-cols-2 lg:grid-cols-[unset] lg:grid-flow-col gap-sp-8">
+<div class="grid justify-around grid-cols-2 lg:grid-cols-[unset] lg:grid-flow-col gap-sp-8 items-center">
   <?php if (have_rows('images')) :
     while (have_rows('images')) : the_row(); ?>
-      <img src="<?= get_sub_field('image')['sizes']['card-thumb']; ?>" alt="<?= get_sub_field('image')['alt'] ?>" class="object-contain aspect-video h-sp-16">
+      <img src="<?= get_sub_field('image')['sizes']['card-thumb']; ?>" alt="<?= get_sub_field('image')['alt'] ?>" class="object-contain aspect-video">
   <?php endwhile;
   endif; ?>
 </div>
