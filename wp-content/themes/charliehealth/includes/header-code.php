d4fb04f9508e314fac0f05b5eb4d1928537f649d
amacaulay <adam.macaulay@charliehealth.com>
2024-01-03 12:21:07 -0500

    add fp gtm code

--- wp-content/themes/charliehealth/includes/header-code.php
+++ wp-content/themes/charliehealth/includes/header-code.php
@@ -90,6 +90,25 @@
 </script>
 <!-- End Google Tag Manager PROD -->
 
+<!-- Google Tag Manager FRESHPAINT TESTING -->
+<script>
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src =
+      'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-WTZ6L3HD');
+</script>
+<!-- End Google Tag Manager FRESHPAINT TESTING -->
+
 <!-- Start VWO Async SmartCode -->
 <link rel="preconnect" href="https://dev.visualwebsiteoptimizer.com" />
 <script type='text/javascript' id='vwoCode'>
