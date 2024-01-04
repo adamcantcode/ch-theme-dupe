aeee2958fe3f76eb304d6ca94986784c31b443a8
amacaulay <adam.macaulay@charliehealth.com>
2024-01-04 15:11:49 -0500

    working marquee

--- wp-content/themes/charliehealth/build/blocks/top-startup/top-startup.php
+++ wp-content/themes/charliehealth/build/blocks/top-startup/top-startup.php
@@ -10,48 +10,61 @@
           <p class="max-w-[200px] mb-0 text-center lg:text-[20px] text-[18px] leading-[1.1]">Selected as a 2023 LinkedIn Top Startup</p>
         </div>
         <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="absolute right-sp-6 bottom-sp-4">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="#161A3D" />
-          </svg>
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="#161A3D" />
+        </svg>
       </div>
     </div>
   </div>
-  <div id="marquee" class="flex justify-between">
-    <img src="https://placehold.co/150x80" alt="">
-    <img src="https://placehold.co/150x80" alt="">
-    <img src="https://placehold.co/150x80" alt="">
-    <img src="https://placehold.co/150x80" alt="">
-    <img src="https://placehold.co/150x80" alt="">
-    <img src="https://placehold.co/150x80" alt="">
+  <div id="marquee" class="flex">
+    <div class="marquee-content scroll reverse">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+    </div>
+    <div class="marquee-content scroll reverse">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+      <img src="https://placehold.co/150x80" alt="">
+    </div>
   </div>
 </section>
-<div class="grid grid-cols-1 gap-sp-5 lg:grid-cols-4">
-  <h2 class="font-heading-serif lg:text-[40px] text-[32px] leading-[1.1] text-white self-center">Our values</h2>
-  <?php include(get_template_directory() . '/includes/button-group.php'); ?>
-  <div class="bg-pale-blue-200 rounded-[6px] p-sp-5 flex flex-col justify-between">
-    <h3 class="font-heading lg:text-[40px] text-[32px] leading-[1.1]">Connection</h3>
-    <div>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Care deeply</h4>
-      <p class="text-[14px] leading-[1.4] mb-sp-8">We care personally about every single person in the Charlie Health ecosystem: our clients, providers, and team members alike.</p>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Inspire hope</h4>
-      <p class="mb-0 text-[14px] leading-[1.4]">We inspire hope with every interaction, reminding our clients that we truly and unconditionally believe in them.</p>
-    </div>
-  </div>
-  <div class="bg-lavender-300 rounded-[6px] p-sp-5 flex flex-col justify-between">
-    <h3 class="font-heading lg:text-[40px] text-[32px] leading-[1.1]">Congruence</h3>
-    <div>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Stay curious</h4>
-      <p class="text-[14px] leading-[1.4] mb-sp-8">We ask “why” five times before we’re satisfied with the answer. We don’t stick to the status quo; we challenge our assumptions and remain humble.</p>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Heed the evidence</h4>
-      <p class="mb-0 text-[14px] leading-[1.4]">Above all, we’re results-oriented. When we find data that calls our original plan into question, we modify or pivot.</p>
-    </div>
-  </div>
-  <div class="bg-yellow-300 rounded-[6px] p-sp-5 flex flex-col justify-between">
-    <h3 class="font-heading lg:text-[40px] text-[32px] leading-[1.1]">Commitment</h3>
-    <div>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Act with urgency</h4>
-      <p class="text-[14px] leading-[1.4] mb-sp-8">We work as swiftly as possible. The mental health crisis is relentless, and so are we.</p>
-      <h4 class="font-heading lg:text-[28px] text-[20px] leading-[1.1] mb-sp-3">Don’t give up</h4>
-      <p class="mb-0 text-[14px] leading-[1.4]">Our clients don’t give up and neither do we. Persistence is our superpower.</p>
-    </div>
-  </div>
-</div>
+
+<style>
+  @keyframes scroll {
+    from {
+      transform: translateX(0);
+    }
+
+    to {
+      transform: translateX(calc(-100% - 1rem));
+    }
+  }
+
+  .scroll {
+    animation: scroll 10s linear infinite;
+  }
+
+  .reverse {
+    animation-direction: reverse;
+  }
+
+  #marquee {
+    width: 100%;
+    display: flex;
+    overflow: hidden;
+  }
+
+  #marquee:hover * {
+    animation-play-state: paused;
+  }
+
+  .marquee-content {
+    display: flex;
+    justify-content: space-around;
+    min-width: 100%;
+  }
+</style>
