9f23e8a425419c641c05b59c6251947caed90ac7
amacaulay <adam.macaulay@charliehealth.com>
2024-01-18 14:20:40 -0500

    nice

--- wp-content/themes/charliehealth/page-press-new.php
+++ wp-content/themes/charliehealth/page-press-new.php
@@ -20,28 +20,9 @@ Template Name: Press page new
       <div class="grid lg:grid-cols-2 gap-sp-5 mb-sp-5">
         <?php
         $featured = get_field('featured_posts');
-        var_dump($featured);
-        ?>
-        <?php
-        $args = array(
-          'post_type'      => 'press',
-          'posts_per_page' => 2,
-          'meta_key'       => 'date',
-          'orderby'        => 'meta_value',
-          'order'          => 'DESC',
-          'meta_query'     => array(
-            array(
-              'key'     => 'featured',
-              'value'   => true,
-              'compare' => '=',
-            )
-          )
-        );
+        $featured = array_slice($featured, 0, 2);
 
-        $query = new WP_Query($args);
-
-        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-            <?php
+        if ($featured) :  foreach ($featured as $post) : setup_postdata($post);
             if (has_post_thumbnail()) {
               $featuredImageID = get_post_thumbnail_id();
               $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
@@ -55,7 +36,7 @@ Template Name: Press page new
             }
             $link = get_field('link');
             $date = get_field('date');
-            ?>
+        ?>
             <div class="relative grid rounded-[6px] overflow-hidden bg-white">
               <div class="grid items-center justify-center w-full h-[190px]">
                 <div class="h-[190px]">
@@ -68,9 +49,43 @@ Template Name: Press page new
               </div>
             </div>
         <?php wp_reset_postdata();
-          endwhile;
-        endif
+          endforeach;
+        endif; ?>
+      </div>
+      <div class="grid lg:grid-cols-4 gap-sp-5 mb-sp-5">
+        <?php
+        $featured = get_field('featured_posts');
+        $featured = array_slice($featured, 2, 6);
 
+        if ($featured) :  foreach ($featured as $post) : setup_postdata($post);
+            if (has_post_thumbnail()) {
+              $featuredImageID = get_post_thumbnail_id();
+              $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+              $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+
+              $featuredImageUrl = $featuredImage[0];
+              $featuredImageAltText = $featuredImageAltText ?: '';
+            } else {
+              $featuredImageUrl = placeHolderImage(600, 800);
+              $featuredImageAltText = 'place holder image';
+            }
+            $link = get_field('link');
+            $date = get_field('date');
+        ?>
+            <div class="relative flex flex-col rounded-[6px] overflow-hidden bg-white">
+              <div class="flex items-center justify-center w-full h-[170px]">
+                <div class="h-[170px]">
+                  <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover w-full h-full">
+                </div>
+              </div>
+              <div class="flex flex-col p-sp-5">
+                <h3 class="mb-sp-3 text-[24px] font-heading  leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary" target="_blank"><?= get_the_title(); ?></a></h3>
+                <p class="mb-0 text-[14px] leading-[1.5]">Publication — <?= $date; ?></p>
+              </div>
+            </div>
+        <?php wp_reset_postdata();
+          endforeach;
+        endif;
         ?>
       </div>
       <div class="grid lg:grid-cols-4 gap-sp-5">
