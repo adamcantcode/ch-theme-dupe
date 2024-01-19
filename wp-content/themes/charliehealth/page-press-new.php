838dd0e4a7d091b96808e54f72a4644f541bc4d7
amacaulay <adam.macaulay@charliehealth.com>
2024-01-19 11:32:17 -0500

    okay

--- wp-content/themes/charliehealth/page-press-new.php
+++ wp-content/themes/charliehealth/page-press-new.php
@@ -37,16 +37,16 @@ Template Name: Press page new
             $date = get_field('date');
             $bgImage = get_field('bg_image') ?: placeHolderImage();
         ?>
-            <div class="relative grid rounded-[6px] overflow-hidden bg-white group">
+            <div class="relative rounded-[6px] overflow-hidden bg-white group">
               <div class="h-[260px] relative">
                 <div class="absolute inset-0 w-full h-full transition-all bg-primary opacity-70 group-hover:opacity-0"></div>
                 <div class="h-full bg-cover" style="background-image: url(<?= $bgImage; ?>)">
                   <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="absolute inset-0 m-auto max-h-[50px] max-w-[200px]">
                 </div>
               </div>
-              <div class="grid p-sp-4">
-                <h3 class="mb-sp-3 text-[24px] font-heading leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary featured-title-js" target="_blank"><?= get_the_title(); ?></a></h3>
-                <p class="mb-0 text-[14px] leading-[1.5]">Publication — <?= $date; ?></p>
+              <div class="p-sp-4">
+                <p class="mb-sp-2 text-[14px] leading-[1.5]"><?= $date; ?></p>
+                <h3 class="mb-0 text-[24px] font-heading leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary featured-title-js" target="_blank"><?= get_the_title(); ?></a></h3>
               </div>
             </div>
         <?php wp_reset_postdata();
@@ -81,15 +81,16 @@ Template Name: Press page new
             $link = get_field('link');
             $date = get_field('date');
         ?>
-            <div class="relative flex flex-col rounded-[6px] overflow-hidden bg-white">
-              <div class="flex items-center justify-center w-full h-[170px]">
-                <div class="h-[170px]">
-                  <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover w-full h-full">
+            <div class="relative rounded-[6px] overflow-hidden bg-white group press-posts-load-more-js">
+              <div class="h-[170px] relative">
+                <div class="absolute inset-0 w-full h-full transition-all bg-primary opacity-70 group-hover:opacity-0"></div>
+                <div class="h-full bg-cover" style="background-image: url(<?= $bgImage; ?>)">
+                  <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="absolute inset-0 m-auto max-h-[50px] max-w-[200px]">
                 </div>
               </div>
-              <div class="flex flex-col p-sp-5">
-                <h3 class="mb-sp-3 text-[24px] font-heading  leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary" target="_blank"><?= get_the_title(); ?></a></h3>
-                <p class="mb-0 text-[14px] leading-[1.5]">Publication — <?= $date; ?></p>
+              <div class="p-sp-4">
+                <p class="mb-sp-2 text-[14px] leading-[1.5]"><?= $date; ?></p>
+                <h3 class="mb-0 text-[24px] font-heading leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary featured-title-js" target="_blank"><?= get_the_title(); ?></a></h3>
               </div>
             </div>
         <?php wp_reset_postdata();
@@ -99,33 +100,54 @@ Template Name: Press page new
       </div>
     </div>
   </section>
-  <section class="section-horizontal">
+  <section class="section bg-grey-warm">
     <div class="container">
-      <h2>Latest Announcements</h2>
-      <div class="divider"></div>
-    </div>
-  </section>
-  <section class="section">
-    <div class="container">
-      <div class="absolute invisible opacity-0 no-posts-js">
-        <div class="grid items-center grid-cols-1 duration-300 rounded-md justify-items-center bg-cream lg:grid-cols-2 p-sp-4">
-          <h4 class="mb-0">There aren't any posts that match this tag. Try again with another tag</h4>
-          <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/icons/not-found.svg'); ?>" alt="not found icon">
-        </div>
+      <h2 class="mb-12 text-[20px] leading-[1.1]">Our Expert Opinion</h2>
+      <div class="grid lg:grid-cols-4 gap-sp-5">
+        <?php
+        $args = array(
+          'post_type'      => 'press',
+          'posts_per_page' => -1,
+          'numberposts'    => -1,
+          'meta_key'       => 'date',
+          'orderby'        => 'meta_value',
+          'order'          => 'DESC',
+        );
+
+        $query = new WP_Query($args);
+
+        if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+            if (has_post_thumbnail()) {
+              $featuredImageID = get_post_thumbnail_id();
+              $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
+              $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
+
+              $featuredImageUrl = $featuredImage[0];
+              $featuredImageAltText = $featuredImageAltText ?: '';
+            } else {
+              $featuredImageUrl = placeHolderImage(600, 800);
+              $featuredImageAltText = 'place holder image';
+            }
+            $link = get_field('link');
+            $date = get_field('date');
+        ?>
+            <div class="relative rounded-[6px] overflow-hidden bg-white group">
+              <div class="h-[170px] relative">
+                <div class="absolute inset-0 w-full h-full transition-all bg-primary opacity-70 group-hover:opacity-0"></div>
+                <div class="h-full bg-cover" style="background-image: url(<?= $bgImage; ?>)">
+                  <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="absolute inset-0 m-auto max-h-[50px] max-w-[200px]">
+                </div>
+              </div>
+              <div class="p-sp-4">
+                <p class="mb-sp-2 text-[14px] leading-[1.5]"><?= $date; ?></p>
+                <h3 class="mb-0 text-[24px] font-heading leading-[1.3]"><a href="<?= $link; ?>" class="stretched-link hover:text-primary featured-title-js" target="_blank"><?= get_the_title(); ?></a></h3>
+              </div>
+            </div>
+        <?php wp_reset_postdata();
+          endwhile;
+        endif;
+        ?>
       </div>
-      <div class="grid posts-container gap-x-sp-8 gap-y-sp-10 mb-sp-10">
-        <!-- <div class="relative grid lg:grid-cols-[1fr_4fr] grid-cols-[1fr_2fr] overflow-hidden border rounded-sm border-card-border">
-          <img srcsp-3"https://images.placeholders.dev /?width=800&height=600&text=FPO" alt="" class="object-contain h-[125px] w-full">
-          <div class="grid p-sp-4">
-           <p class="mb-0 text-[14px] leading-[1.5]"><?php // $date; 
-                                                      ?></p>
-            <h3 class="mb-0"><a href="<?php // get_the_ID(); 
-                                      ?>" class="stretched-link"><?php // get_the_title(); 
-                                                                  ?></a></h3>
-          </div>
-        </div> -->
-      </div>
-      <div class="pagination-container"></div>
     </div>
   </section>
 </article>
