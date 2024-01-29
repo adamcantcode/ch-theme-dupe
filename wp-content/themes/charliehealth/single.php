c8e0a383a44cdb05999041c710abedeaa264a93e
amacaulay <adam.macaulay@charliehealth.com>
2024-01-29 14:06:36 -0500

    show/hide

--- wp-content/themes/charliehealth/single.php
+++ wp-content/themes/charliehealth/single.php
@@ -2,37 +2,37 @@
 
 <?php
 $protocol = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
-$domain = $_SERVER['HTTP_HOST'];
-$path = $_SERVER['REQUEST_URI'];
-$fullUrl = $protocol . $domain . $path;
+$domain   = $_SERVER['HTTP_HOST'];
+$path     = $_SERVER['REQUEST_URI'];
+$fullUrl  = $protocol . $domain . $path;
 
 if (has_post_thumbnail()) {
-  $featuredImageID = get_post_thumbnail_id();
-  $featuredImage = wp_get_attachment_image_src($featuredImageID, 'featured-large');
+  $featuredImageID      = get_post_thumbnail_id();
+  $featuredImage        = wp_get_attachment_image_src($featuredImageID, 'featured-large');
   $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
 
-  $featuredImageUrl = $featuredImage[0];
+  $featuredImageUrl     = $featuredImage[0];
   $featuredImageAltText = $featuredImageAltText ?: '';
 } else {
-  $featuredImageUrl = site_url('/wp-content/uploads/2023/06/charlie-health_find-your-group.png.webp');
+  $featuredImageUrl     = site_url('/wp-content/uploads/2023/06/charlie-health_find-your-group.png.webp');
   $featuredImageAltText = 'Charlie Health Logo';
 }
 
-$author = get_field('by_author');
+$author          = get_field('by_author');
 $medicalReviewer = get_field('medical_reviewer');
-$date = get_field('date') ?: '';
-$updatedDate = get_field('select_updated_date') ?: '';
-$relatedPosts = get_field('related_posts') ?: '';
-$toc = get_field('toc') ?: '';
-$references = get_field('references') ?: '';
+$date            = get_field('date') ?: '';
+$updatedDate     = get_field('select_updated_date') ?: '';
+$relatedPosts    = get_field('related_posts') ?: '';
+$toc             = get_field('toc') ?: '';
+$prefooter       = get_field('prefooter_cta') ?: '';
+$references      = get_field('references') ?: '';
 
 $audiences = get_the_terms(get_the_ID(), 'category');
-$tags = get_the_terms(get_the_ID(), 'post_tag');
+$tags      = get_the_terms(get_the_ID(), 'post_tag');
 
-$wordCount = str_word_count(strip_tags(get_the_content()));
+$wordCount      = str_word_count(strip_tags(get_the_content()));
 $wordsPerMinute = 238;
-$readingTime = ceil($wordCount / $wordsPerMinute);
-
+$readingTime    = ceil($wordCount / $wordsPerMinute);
 ?>
 
 <div id="progressBar" class="fixed z-20 transition-all duration-700 rounded-r-sm bg-purple-gradient-end h-[5px] left-0"></div>
@@ -138,7 +138,7 @@ $readingTime = ceil($wordCount / $wordsPerMinute);
       <div class="divider"></div>
     </div>
   </section>
-  <article id="articleContent" class="section-xs-top">
+  <article id="articleContent" class="section">
     <div class="container-sm">
       <?php the_content(); ?>
       <?php if ($references) : ?>
@@ -163,9 +163,11 @@ $readingTime = ceil($wordCount / $wordsPerMinute);
     </div>
   </article>
 </div>
-<?= do_blocks('<!-- wp:block {"ref":12} /-->'); ?>
+<?php if (!$prefooter) : ?>
+  <?= do_blocks('<!-- wp:block {"ref":12} /-->'); ?>
+<?php endif; ?>
 <?php if ($relatedPosts) : ?>
-  <section class="section-horizontal">
+  <section class="section-bottom">
     <div class="container">
       <h2>More like this</h2>
       <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10">
