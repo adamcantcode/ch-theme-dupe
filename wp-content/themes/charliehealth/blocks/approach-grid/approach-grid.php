5e9179ffb62501d12e442252f66453499d0edc35
amacaulay <adam.macaulay@charliehealth.com>
2024-02-14 16:59:29 -0500

    grids grids grids

--- wp-content/themes/charliehealth/blocks/approach-grid/approach-grid.php
+++ wp-content/themes/charliehealth/blocks/approach-grid/approach-grid.php
@@ -1,4 +1,4 @@
-<div class="border-2 border-white rounded-[1rem] lg:p-sp-12 p-sp-4 grid lg:grid-cols-[4fr_1fr_7fr] gap-x-sp-5 approach-grid-js mt-s">
+<div class="border-2 border-white rounded-[1rem] lg:p-sp-12 p-sp-4 grid lg:grid-cols-[4fr_8fr] gap-x-sp-5 approach-grid-js">
   <div class="grid content-between">
     <div class="order-2 lg:order-1">
       <h2 class="text-white font-heading-serif mb-sp-8"><?= get_field('heading'); ?></h2>
@@ -14,7 +14,6 @@
       </svg>
     </div>
   </div>
-  <div></div>
   <?php if (have_rows('grid')) :  ?>
     <?php
     $bgColors = [
