e54cc0d3252d96070beb9e79a56ec37c4eaaf97d
amacaulay <adam.macaulay@charliehealth.com>
2024-02-21 17:40:43 -0500

    fields

--- wp-content/themes/charliehealth/build/blocks/job-board-peek/job-board-peek.php
+++ wp-content/themes/charliehealth/build/blocks/job-board-peek/job-board-peek.php
@@ -1,19 +1,19 @@
-<div id="jobListings" class="min-h-[100vh]">
+<div id="jobListings">
   <div class="grid grid-cols-1 gap-x-sp-8 lg:grid-cols-[3fr_9fr] mt-sp-16 first:mt-0 job-departments-section-js transition-all duration-300">
     <div class="job-departments-js">
       <h6>Clinical</h6>
       <h3 class="text-[40px] leading-[1.1] font-heading my-sp-4">Open roles at Charlie Health</h3>
     </div>
-    <div class="job-list-js">
-      <div class="relative flex flex-col justify-between transition-all duration-300 border-b lg:flex-row lg:items-center border-primary first:border-t py-sp-6 gap-x-sp-5 job-list-job-js">
-        <a href="https://www.charliehealth.com/careers/clinical/current-clinical-openings?gh_jid=5022688004" class="no-underline stretched-link text-[20px] leading-[1.1] mb-sp-2 lg:mb-0">Care Coach (Part-Time) - Spanish Speaking </a>
-        <p class="mb-0 text-[14px] leading-[1.1] location-js lg:text-right">Remote, United States</p>
+    <div>
+      <div class="job-list-js"></div>
+      <div class="grid justify-end mt-sp-8">
+        <?php include(get_template_directory() . '/includes/button-group.php'); ?>
       </div>
     </div>
   </div>
 </div>
 
-<?php $boardCode = 'charliehealth'; ?>
+<?php $boardCode = get_field('board_code'); ?>
 <script>
   window.addEventListener('DOMContentLoaded', () => {
     var requestOptions = {
@@ -21,25 +21,22 @@
       redirect: 'follow'
     };
 
-    fetch("https://boards-api.greenhouse.io/v1/boards/<?= $boardCode; ?>/departments", requestOptions)
+    fetch("https://boards-api.greenhouse.io/v1/boards/<?= $boardCode; ?>/jobs", requestOptions)
       .then(response => response.json())
       .then(data => {
-        // Save the fetched data for later use
-        var departmentsData = data;
-        var jobsContainer = document.querySelector('.job-list-job-js');
+        var jobsContainer = document.querySelector('.job-list-js');
 
         // Build markup
-        departmentsData.departments.forEach(department => {
-          // Check if the department has jobs
-          if (department.jobs.length > 0) {
-            const jobMarkup = department.jobs.map(job => `
+        data.jobs.forEach(department => {
+          if (data.jobs.length > 0) {
+            const jobMarkup = data.jobs.slice(0, 5).map(job => `
                   <div class="relative flex flex-col justify-between transition-all duration-300 border-b lg:flex-row lg:items-center border-primary first:border-t py-sp-6 gap-x-sp-5 job-list-job-js">
                     <a href="${job.absolute_url}" class="no-underline stretched-link text-[20px] leading-[1.1] mb-sp-2 lg:mb-0">${job.title}</a>
                     <p class="mb-0 text-[14px] leading-[1.1] location-js lg:text-right">${job.location.name}</p>
                   </div>
                 `).join('');
 
-            jobsContainer.innerHTML = markup;
+            jobsContainer.innerHTML = jobMarkup;
 
             setTimeout(() => {
               jobsContainer.classList.remove('opacity-0');
