2a52d793d43a3fab04af5faa95438865e471b0dd
amacaulay <adam.macaulay@charliehealth.com>
2024-02-28 12:41:31 -0500

    fields

--- wp-content/themes/charliehealth/page-condition-treatment.php
+++ wp-content/themes/charliehealth/page-condition-treatment.php
@@ -81,26 +81,6 @@ $readingTime    = ceil($wordCount / $wordsPerMinute);
   <div class="invisible opacity-0 noshow back-to-top top-[150px] left-sp-10 mb-sp-16 w-fit translate-y-base5-3">
     <a href="#mainArticleContent" class="no-underline text-h3-base font-heading-serif">Back to top</a>
   </div>
-  <?php if ($toc) : ?>
-    <section class="section-xs">
-      <div class="container-sm">
-        <div class="rounded-md toc-container bg-light-purple">
-          <div class="flex cursor-pointer toc-heading lg:p-sp-8 p-sp-4">
-            <p class="mb-0 text-h3-base">Table of Contents</p>
-            <div class="flex items-center ml-auto toggle">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" preserveAspectRatio="none" viewBox="8 8 8 8" height="12px" width="12px">
-                <path d="M9 12H15" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                <path d="M12 9L12 15" stroke="#323232" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-              </svg>
-            </div>
-          </div>
-          <div class="overflow-hidden transition-all duration-500 ease-in-out toc-content max-h-0">
-            <div id="toc" class="flex flex-col items-start pt-0 lg:pt-0 lg:p-sp-8 p-sp-4 gap-sp-1"></div>
-          </div>
-        </div>
-      </div>
-    </section>
-  <?php endif; ?>
   <section class="section-horizontal">
     <div class="container">
       <div class="divider"></div>
@@ -133,47 +113,6 @@ $readingTime    = ceil($wordCount / $wordsPerMinute);
 </div>
 <?php if (!$prefooter) : ?>
   <?= do_blocks('<!-- wp:block {"ref":12} /-->'); ?>
-<?php endif; ?>
-<?php if ($relatedPosts) : ?>
-  <section class="section bg-grey-cool">
-    <div class="container">
-      <h2>More like this</h2>
-      <div class="grid lg:grid-cols-3 posts-container gap-x-sp-8 gap-y-sp-10">
-        <?php foreach ($relatedPosts as $post) : ?>
-          <?php
-          if (has_post_thumbnail()) {
-            $featuredImageID = get_post_thumbnail_id();
-            $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
-            $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
-
-            $featuredImageUrl = $featuredImage[0];
-            $featuredImageAltText = $featuredImageAltText ?: '';
-          } else {
-            $featuredImageUrl = site_url('/wp-content/uploads/2023/06/charlie-health_find-your-group.png.webp');
-            $featuredImageAltText = 'Charlie Health Logo';
-          }
-          ?>
-          <div class="relative bg-white rounded-lg group">
-            <div class="lg:h-[167px] h-[150px] overflow-hidden rounded-t-lg">
-              <img src="<?= $featuredImageUrl; ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover w-full h-full transition-all duration-300 rounded-t-lg group-hover:scale-105">
-            </div>
-            <div class="absolute rounded-t-lg top-sp-4 left-sp-4">
-              <?php $tags = get_the_terms(get_the_ID(), 'post_tag');  ?>
-              <?php if ($tags) :  ?>
-                <?php foreach ($tags as $tag) : ?>
-                  <a href="<?= get_term_link($tag->slug, 'post_tag'); ?>" class="relative inline-block no-underline rounded-pill px-base5-3 py-base5-2 text-white bg-transparent group-hover:bg-white group-hover:!text-primary border border-white z-[6] text-h5-base"><?= $tag->name; ?></a>
-                <?php endforeach; ?>
-              <?php endif; ?>
-            </div>
-            <div class="grid bg-white rounded-b-lg p-sp-4">
-              <h3 class="text-h4-base"><a href="<?= get_the_permalink(); ?>" class="block stretched-link"><?= get_the_title(); ?></a></h3>
-              <p><?= $author->post_title; ?></p>
-            </div>
-          </div>
-        <?php endforeach; ?>
-      </div>
-    </div>
-  </section>
 <?php endif; ?>
 <?= do_blocks('<!-- wp:block {"ref":7353} /-->'); ?>
 
