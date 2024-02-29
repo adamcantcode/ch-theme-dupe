6561a6fb7e56cc5c50cb343be679898c51d9245d
amacaulay <adam.macaulay@charliehealth.com>
2024-02-29 12:19:15 -0500

    subhead and cta cleanup

--- wp-content/themes/charliehealth/build/blocks/scrolling-cta/scrolling-cta.php
+++ wp-content/themes/charliehealth/build/blocks/scrolling-cta/scrolling-cta.php
@@ -23,13 +23,16 @@ if (!empty($block['align'])) {
     </div>
   </div>
   <div class="section section-bg-js-cta">
-    <div class="container-sm">
+    <div class="container">
       <div class="flex flex-col justify-center pin-cta-js lg:h-[50vh] lg:mt-0 mt-sp-16 pb-sp-14 lg:pb-0">
         <div class="pin-cta-js-motion">
           <div class="flex justify-center rounded-sm lg:px-sp-14 lg:pt-sp-14 pb-sp-6 px-sp-6">
-            <div class="flex flex-col items-center justify-center text-center max-w-[700px]">
+            <div class="flex flex-col items-center justify-center text-center max-w-[720px]">
               <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health shield logo" class="w-[3rem] mb-sp-5">
-              <h2 class="text-darkest-blue lg:text-[2.5rem] text-h2-lg lg:leading-tight mb-sp-10 font-heading-serif"><?= get_field('heading'); ?></h2>
+              <p class="text-h2-base"><?= get_field('heading'); ?></p>
+              <?php if (!empty(get_field('subhead'))) : ?>
+                <p class="text-h3-base"><?= get_field('subhead'); ?></p>
+              <?php endif; ?>
               <?php include(get_template_directory() . '/includes/button-group.php'); ?>
             </div>
           </div>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8989769546091e819832');
+<?php return array('dependencies' => array(), 'version' => '4bd714dabdb3d470f895');
