d9b7f0d68eb3e46befaabf8d7da0ad1e38b9ba7d
amacaulay <adam.macaulay@charliehealth.com>
2024-03-05 17:09:20 -0500

    blog posts nl logic

--- wp-content/themes/charliehealth/single.php
+++ wp-content/themes/charliehealth/single.php
@@ -62,7 +62,7 @@ $readingTime    = ceil($wordCount / $wordsPerMinute);
           <p><?= get_the_excerpt(); ?></p>
           <p class="mb-0">By: <a href="<?= get_the_permalink($author->ID); ?>"><?= $author->post_title; ?></a></p>
           <?php if (!empty($medicalReviewer)) : ?>
-            <p class="mb-0">Clinically Reviewed By: <a href="<?=  get_the_permalink($medicalReviewer->ID); ?>"><?= $medicalReviewer->post_title; ?></a></p>
+            <p class="mb-0">Clinically Reviewed By: <a href="<?= get_the_permalink($medicalReviewer->ID); ?>"><?= $medicalReviewer->post_title; ?></a></p>
           <?php endif; ?>
           <?php if (!$updatedDate) : ?>
             <p><?= $date; ?></p>
@@ -208,5 +208,44 @@ $readingTime    = ceil($wordCount / $wordsPerMinute);
   </section>
 <?php endif; ?>
 <?= do_blocks('<!-- wp:block {"ref":7353} /-->'); ?>
+<?php
+$newsletterImage = get_field('image', 'option');
+$headline = get_field('headline', 'option');
+$subhead = get_field('subhead', 'option');
+?>
+<div id="newsletterPopupBlogPost" class="bg-[rgba(0,0,0,.5)] fixed top-0 left-0 w-full h-full z-50 grid items-center justify-center center transition-all duration-300 modal-fade">
+  <div class="transition-all duration-300 section-xs">
+    <div class="grid lg:grid-cols-[1fr,2fr] bg-cream container max-h-[80vh] overflow-auto rounded-md items-center relative">
+      <div class="absolute top-0 right-0 cursor-pointer">
+        <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/close-x.svg'); ?>" alt="close button" class="w-full duration-300 modal-close p-sp-4 hover:brightness-0">
+      </div>
+      <img src="<?= $newsletterImage['sizes']['featured-large']; ?>" alt="<?= $newsletterImage['alt']; ?>" class="object-cover w-full h-full noshow lg:block">
+      <div class="p-sp-8">
+        <h2 class="text-h1-base font-heading"><?= $headline; ?></h2>
+        <p class="h-full lg:block"><?= $subhead; ?></p>
+        <div id="newsletterPopupBlogPost" class="newsletter-revamp">
+          <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/newsletter_blog_revamp"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/newsletter_blog_revamp" title="Online Form">Online Form - Newsletter - Blog Revamp</a></noscript>
+          <script>
+            var container = document.currentScript.parentNode; // Newsletter container
+            var elementToCut = container.querySelector("#fsSubmitButton5194985"); // Submit button
+            var destinationElement = container.querySelector("#fsCell140490700"); // Email container
+            var newsletterID = container.id; // Newlsetter identifier
+            var newsletterLPField = container.querySelector('#field142799721'); // LP URL field
+            var newsletterIDField = container.querySelector('#field146376375'); // Type field
 
+            if (elementToCut && destinationElement) {
+              var clonedElement = elementToCut.cloneNode(true);
+              elementToCut.parentNode.removeChild(elementToCut);
+              destinationElement.appendChild(clonedElement);
+            }
+
+            newsletterIDField.value = newsletterID;
+            newsletterLPField.value = window.location.href;
+          </script>
+        </div>
+        <h5>You can unsubscribe anytime.</h5>
+      </div>
+    </div>
+  </div>
+</div>
 <?php get_footer(); ?>
