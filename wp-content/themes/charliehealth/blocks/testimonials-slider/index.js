8a9b421d2d37bab91b5b3456e91ca47b334d22e6
amacaulay <adam.macaulay@charliehealth.com>
2024-03-07 15:23:03 -0500

    init

---
+++ wp-content/themes/charliehealth/blocks/testimonials-slider/index.js
@@ -0,0 +1,36 @@
+import './index.css';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+window.addEventListener('load', () => {
+  var swiper = new Swiper('.swiper.swiper-careers-testimonial', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    slideToClickedSlide: true,
+    speed: 1000,
+    loop: false,
+    breakpoints: {
+      1024: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-testimonial',
+      prevEl: '.swiper-button-prev-testimonial',
+    },
+    on: {
+      reachEnd: function () {
+        this.snapGrid = [...this.slidesGrid];
+        setTimeout(() => {
+          document.querySelector('.swiper-button-next-testimonial').click();
+          clearTimeout();
+        }, 1);
+      },
+    },
+  });
+});
