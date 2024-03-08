8ab2dde3441507bbb8f6da5aa7e9666dc1b9d34a
amacaulay <adam.macaulay@charliehealth.com>
2024-03-08 12:23:30 -0500

    uhhh this is something

--- wp-content/themes/charliehealth/blocks/child-pages/child-pages.php
+++ wp-content/themes/charliehealth/blocks/child-pages/child-pages.php
@@ -1,7 +1,7 @@
-<div class="grid lg:grid-cols-4 gap-sp-5">
+<div class="grid lg:grid-cols-3 gap-sp-5">
   <?php
   $args = array(
-    'post_type'      => 'page',
+    'post_type'      => array('areas-of-care', 'treatment-modalities', 'page'),
     'posts_per_page' => -1,
     'numberposts'    => -1,
     'post_parent'    => get_the_ID(),
@@ -25,13 +25,12 @@
       }
       // $date = get_field('date');
   ?>
-      <div class="relative rounded-[6px] overflow-hidden bg-white group not-loaded noshow opacity-0 press-posts-js transition-all duration-500">
+      <div class="relative rounded-[6px] overflow-hidden bg-white group not-loaded noshow opacity-0 child-posts-js transition-all duration-500 border border-primary">
         <div class="h-[170px] relative overflow-hidden">
-          <div class="absolute inset-0 w-full h-full transition-all duration-300 bg-primary opacity-[.75] group-hover:opacity-0 z-[2]"></div>
           <img src="<?= $featuredImageUrl ?: placeHolderImage(); ?>" alt="<?= $featuredImageAltText; ?>" class="object-cover object-top w-full h-full transition-all duration-300 bg-cover group-hover:scale-105">
         </div>
         <div class="p-sp-4">
-          <h3><a href="<?= get_the_permalink() ?>" class="stretched-link hover:text-primary line-clamp-3"><?= get_the_title(); ?></a></h3>
+          <h4><a href="<?= get_the_permalink() ?>" class="stretched-link hover:text-primary line-clamp-3"><?= get_the_title(); ?></a></h4>
         </div>
       </div>
   <?php wp_reset_postdata();
@@ -40,5 +39,34 @@
   ?>
 </div>
 <div class="flex">
-  <a role="button" class="w-full ml-auto ch-button button-primary justify-self-center lg:w-auto press-load-more-js lg:mt-sp-10 mt-sp-5 mb-sp-5 lg:mb-0">Load more</a>
+  <a role="button" class="w-full ml-auto ch-button button-primary justify-self-center lg:w-auto child-load-more-js lg:mt-sp-10 mt-sp-5 mb-sp-5 lg:mb-0">Load more</a>
 </div>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const loadMoreChildPages = document.querySelector('.child-load-more-js');
+    const posts = document.querySelectorAll('.child-posts-js.not-loaded');
+    const firstFourPosts = Array.from(posts).slice(0, 3);
+
+    if (posts.length < 3) {
+      loadMoreChildPages.remove()
+    }
+
+    firstFourPosts.forEach(post => {
+      post.classList.remove('noshow', 'not-loaded', 'opacity-0');
+    });
+
+    loadMoreChildPages.addEventListener('click', function() {
+      let posts = document.querySelectorAll('.child-posts-js.not-loaded');
+      let firstFourPosts = Array.from(posts).slice(0, 3);
+      firstFourPosts.forEach(post => {
+        post.classList.remove('noshow', 'not-loaded');
+        setTimeout(() => {
+          post.classList.remove('opacity-0');
+        }, 10);
+      });
+      if (document.querySelectorAll('.child-posts-js.not-loaded').length === 0) {
+        loadMoreChildPages.remove()
+      }
+    })
+  })
+</script>
--- wp-content/themes/charliehealth/functions.php
+++ wp-content/themes/charliehealth/functions.php
@@ -136,7 +136,7 @@ function register_acf_blocks()
   register_block_type(__DIR__ . '/build/blocks/job-board-peek');
   register_block_type(__DIR__ . '/build/blocks/check-list-items');
   register_block_type(__DIR__ . '/build/blocks/blue-full-cards');
-  register_block_type(__DIR__ . '/build/blocks/child-pages-cards');
+  register_block_type(__DIR__ . '/build/blocks/child-pages');
 }
 add_action('init', 'register_acf_blocks', 5);
 
