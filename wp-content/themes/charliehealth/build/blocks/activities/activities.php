3becc8639e591f9fa6415d0b34fa5d0c4b754b2d
amacaulay <adam.macaulay@charliehealth.com>
2024-03-12 15:38:53 -0400

    layout

--- wp-content/themes/charliehealth/build/blocks/activities/activities.php
+++ wp-content/themes/charliehealth/build/blocks/activities/activities.php
@@ -2,105 +2,116 @@
 $filterTopics = get_terms('topic');
 $filterTypes  = get_terms('resource-type');
 ?>
-<div class="grid lg:grid-cols-3 gap-base5-4">
-  <div data-topic-featured=".topic-one" class="flex items-center justify-between rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5 bg-pale-blue-100">
-    <p class="inline-block mb-0">test one</p>
-    <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
-    </svg>
-  </div>
-  <div data-topic-featured=".topic-two" class="flex items-center justify-between rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5 bg-lavender-100">
-    <p class="inline-block mb-0">test two</p>
-    <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
-    </svg>
-  </div>
-  <div data-topic-featured=".topic-three" class="flex items-center justify-between bg-yellow-300 rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5">
-    <p class="inline-block mb-0">test three</p>
-    <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
-    </svg>
-  </div>
-</div>
-<div class="grid lg:grid-cols-[1fr_3fr] min-h-screen">
-  <div>
-    <div class="grid button-group filter-button-group">
-      <div class="grid">
-        <p>Resource topic</p>
-        <?php foreach ($filterTopics as $filterTopic) : ?>
-          <label class="flex items-center cursor-pointer text-p-base gap-base5-2"><input type="checkbox" value=".<?= $filterTopic->slug; ?>" class="topic-filter" /><?= $filterTopic->name; ?></label>
-        <?php endforeach; ?>
-      </div>
-      <div class="grid">
-        <p>Resource type</p>
-        <?php foreach ($filterTypes as $filterType) : ?>
-          <label class="flex items-center cursor-pointer text-p-base gap-base5-2"><input type="checkbox" value=".<?= $filterType->slug; ?>" class="type-filter" /><?= $filterType->name; ?></label>
-        <?php endforeach; ?>
+<section class="section bg-primary">
+  <div class="container">
+    <div class="grid gap-base5-4 lg:grid-cols-[1fr_3fr]">
+      <h2 class="text-white">Popular topics</h2>
+      <div class="grid lg:grid-cols-3 gap-base5-4">
+        <div data-topic-featured=".topic-one" class="flex items-center justify-between transition-all rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5 hover:contrast-75 bg-lavender-100">
+          <p class="inline-block mb-0 text-h4-base">test one</p>
+          <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
+          </svg>
+        </div>
+        <div data-topic-featured=".topic-two" class="flex items-center justify-between transition-all rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5 hover:contrast-75 bg-pale-blue-100">
+          <p class="inline-block mb-0 text-h4-base">test two</p>
+          <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
+          </svg>
+        </div>
+        <div data-topic-featured=".topic-three" class="flex items-center justify-between transition-all bg-yellow-100 rounded-md cursor-pointer topic-filter-featured text-h4-base py-base5-4 px-base5-5 hover:contrast-75">
+          <p class="inline-block mb-0 text-h4-base">test three</p>
+          <svg width="20" height="15" viewBox="0 0 20 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M12.0862 0.281473L19.5226 6.39729C19.979 6.77258 19.979 7.38106 19.5226 7.75636L12.0862 13.8722C11.6299 14.2475 10.89 14.2475 10.4337 13.8722C9.97731 13.4969 9.97731 12.8884 10.4337 12.5131L15.8753 8.03783H0V6.11582H15.8753L10.4337 1.64054C9.97731 1.26525 9.97731 0.65677 10.4337 0.281473C10.89 -0.0938243 11.6299 -0.0938243 12.0862 0.281473Z" fill="#161A3D" />
+          </svg>
+        </div>
       </div>
     </div>
-    <div>
-      <input type="text" class="search-input" placeholder="Search..." />
-    </div>
   </div>
-  <div>
-    <div class="flex grid-test">
-      <div class="w-full grid-sizer lg:w-[calc(33.33%_-15px)]"></div>
-      <?php
-      $args = array(
-        'post_type'      => 'activities',
-        'posts_per_page' => -1,
-        'order'          => 'DESC',
-        'orderby'        => 'date',
-        'post_status'    => 'publish'
-      );
-
-      $query = new WP_Query($args);
-
-      if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
-          <?php
-          $topics = get_the_terms(get_the_ID(), 'topic');
-          $types  = get_the_terms(get_the_ID(), 'resource-type');
-          if (is_array($topics)) {
-            $topicName = $topics[0]->name;
-            $topic = [];
-            foreach ($topics as $topicSlug) {
-              $topic[] = $topicSlug->slug;
-            }
-          }
-          if (is_array($types)) {
-            $typeName = $types[0]->name;
-            $type = [];
-            foreach ($types as $typesSlug) {
-              $type[] = $typesSlug->slug;
-            }
-          }
-          ?>
-          <div class="relative w-full mb-base5-4 bg-white rounded-lg group grid-item lg:w-[calc(33.33%_-15px)] <?= implode(' ', $topic); ?> <?= implode(' ', $type); ?>">
-            <div class="lg:h-[167px] h-[150px] overflow-hidden rounded-t-lg">
-              <img src="<?= placeHolderImage(); ?>" alt="#" class="object-cover w-full h-full transition-all duration-300 rounded-t-lg group-hover:scale-105">
-            </div>
-            <?php if ($type) : ?>
-              <div class="absolute rounded-t-lg top-sp-4 left-sp-4">
-                <p class="relative inline-block no-underline rounded-pill px-base5-3 py-base5-2 text-white bg-transparent group-hover:bg-white group-hover:!text-primary border border-white z-[6] text-h5-base cursor-pointer"><?= $typeName; ?></p>
-              </div>
-            <?php endif; ?>
-            <div class="grid bg-white rounded-b-lg p-sp-4">
-              <h3 class="text-h4-base"><a href="<?= get_the_permalink(); ?>" class="block stretched-link"><?= get_the_title(); ?></a></h3>
-              <?php if ($topic) : ?>
-                <p><?= $topicName; ?></p>
-              <?php endif; ?>
-            </div>
+</section>
+<section class="section bg-grey-cool">
+  <div class="container">
+    <div class="grid lg:grid-cols-[1fr_3fr] min-h-screen gap-base5-4">
+      <div>
+        <div class="grid button-group filter-button-group">
+          <div class="grid">
+            <p>Resource topic</p>
+            <?php foreach ($filterTopics as $filterTopic) : ?>
+              <label class="flex items-center cursor-pointer text-p-base gap-base5-2"><input type="checkbox" value=".<?= $filterTopic->slug; ?>" class="topic-filter" /><?= $filterTopic->name; ?></label>
+            <?php endforeach; ?>
           </div>
-      <?php endwhile;
-        wp_reset_query();
-      endif;
-      ?>
-    </div>
-    <div class="grid justify-end">
-      <a role="button" class="w-full ch-button button-primary justify-self-center lg:w-auto load-more-js">Load more</a>
+          <div class="grid">
+            <p>Resource type</p>
+            <?php foreach ($filterTypes as $filterType) : ?>
+              <label class="flex items-center cursor-pointer text-p-base gap-base5-2"><input type="checkbox" value=".<?= $filterType->slug; ?>" class="type-filter" /><?= $filterType->name; ?></label>
+            <?php endforeach; ?>
+          </div>
+        </div>
+        <div>
+          <input type="text" class="search-input" placeholder="Search..." />
+        </div>
+      </div>
+      <div>
+        <div class="flex grid-test">
+          <div class="w-full grid-sizer lg:w-[calc(33.33%_-15px)]"></div>
+          <?php
+          $args = array(
+            'post_type'      => 'activities',
+            'posts_per_page' => -1,
+            'order'          => 'DESC',
+            'orderby'        => 'date',
+            'post_status'    => 'publish'
+          );
+
+          $query = new WP_Query($args);
+
+          if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post(); ?>
+              <?php
+              $topics = get_the_terms(get_the_ID(), 'topic');
+              $types  = get_the_terms(get_the_ID(), 'resource-type');
+              if (is_array($topics)) {
+                $topicName = $topics[0]->name;
+                $topic = [];
+                foreach ($topics as $topicSlug) {
+                  $topic[] = $topicSlug->slug;
+                }
+              }
+              if (is_array($types)) {
+                $typeName = $types[0]->name;
+                $type = [];
+                foreach ($types as $typesSlug) {
+                  $type[] = $typesSlug->slug;
+                }
+              }
+              ?>
+              <div class="relative w-full mb-base5-4 bg-white rounded-lg group grid-item lg:w-[calc(33.33%_-15px)] <?= implode(' ', $topic); ?> <?= implode(' ', $type); ?>">
+                <div class="lg:h-[167px] h-[150px] overflow-hidden rounded-t-lg">
+                  <img src="<?= placeHolderImage(); ?>" alt="#" class="object-cover w-full h-full transition-all duration-300 rounded-t-lg group-hover:scale-105">
+                </div>
+                <?php if ($type) : ?>
+                  <div class="absolute rounded-t-lg top-sp-4 left-sp-4">
+                    <p class="relative inline-block no-underline rounded-pill px-base5-3 py-base5-2 text-white bg-transparent group-hover:bg-white group-hover:!text-primary border border-white z-[6] text-h5-base cursor-pointer"><?= $typeName; ?></p>
+                  </div>
+                <?php endif; ?>
+                <div class="grid bg-white rounded-b-lg p-sp-4">
+                  <h3 class="text-h4-base"><a href="<?= get_the_permalink(); ?>" class="block stretched-link"><?= get_the_title(); ?></a></h3>
+                  <?php if ($topic) : ?>
+                    <p><?= $topicName; ?></p>
+                  <?php endif; ?>
+                </div>
+              </div>
+          <?php endwhile;
+            wp_reset_query();
+          endif;
+          ?>
+        </div>
+        <div class="grid justify-end">
+          <a role="button" class="w-full ch-button button-primary justify-self-center lg:w-auto load-more-js">Load more</a>
+        </div>
+      </div>
     </div>
   </div>
-</div>
+</section>
 
 <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
 <script>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '583d2f8d65698b71e278');
+<?php return array('dependencies' => array(), 'version' => '18febd75aa5e77b25ac1');
