f590ae30666f7ffbfaf6c5762ec8508a721ded42
amacaulay <adam.macaulay@charliehealth.com>
2024-04-09 13:39:07 -0400

    press posts

--- wp-content/themes/charliehealth/build/blocks/press-posts/press-posts.php
+++ wp-content/themes/charliehealth/build/blocks/press-posts/press-posts.php
@@ -1,10 +1,10 @@
-<h2 class="text-h4-base">Charlie Health in the news</h2>
+<h2 class="text-h4-base font-heading mb-base5-6">Charlie Health in the news</h2>
 <div class="grid lg:grid-cols-4 gap-sp-5">
   <?php
   $args = array(
     'post_type'      => 'press',
-    'posts_per_page' => -1,
-    'numberposts'    => -1,
+    'posts_per_page' => 4,
+    'numberposts'    => 4,
     'meta_key'       => 'date',
     'orderby'        => 'meta_value',
     'order'          => 'DESC',
@@ -20,8 +20,9 @@
   $query = new WP_Query($args);
 
   if ($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
-      if (has_post_thumbnail()) {
-        $featuredImageID = get_post_thumbnail_id();
+      global $post;
+      if (has_post_thumbnail($post->ID)) {
+        $featuredImageID = get_post_thumbnail_id($post->ID);
         $featuredImage = wp_get_attachment_image_src($featuredImageID, 'card-thumb');
         $featuredImageAltText = get_post_meta($featuredImageID, '_wp_attachment_image_alt', true);
 
@@ -31,11 +32,11 @@
         $featuredImageUrl = placeHolderImage(600, 800);
         $featuredImageAltText = 'place holder image';
       }
-      $link = get_field('link');
-      $date = get_field('date');
-      $bgImage = get_field('bg_image');
+      $link = get_field('link', $post->ID);
+      $date = get_field('date', $post->ID);
+      $bgImage = get_field('bg_image', $post->ID);
   ?>
-      <div class="relative rounded-[6px] bg-white group not-loaded noshow opacity-0 expert-posts-js transition-all duration-500 overflow-hidden">
+      <div class="relative rounded-[6px] bg-white group overflow-hidden">
         <div class="h-[170px] relative overflow-hidden">
           <div class="absolute inset-0 w-full h-full transition-all duration-300 bg-primary opacity-[.75] group-hover:opacity-0 z-[2]"></div>
           <img src="<?= $bgImage['url'] ?: placeHolderImage(); ?>" alt="<?= $bgImage['alt']; ?>" class="object-cover object-top w-full h-full transition-all duration-300 bg-cover group-hover:scale-105">
@@ -43,7 +44,7 @@
         </div>
         <div class="p-sp-4">
           <p><?= $date; ?></p>
-          <h3><a href="<?= $link; ?>" class="stretched-link hover:text-primary line-clamp-3" target="_blank"><?= get_the_title(); ?></a></h3>
+          <h4><a href="<?= $link; ?>" class="stretched-link hover:text-primary line-clamp-3" target="_blank"><?= get_the_title($post->ID); ?></a></h4>
         </div>
       </div>
   <?php wp_reset_postdata();
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'daabcf4b5de2786103c4');
+<?php return array('dependencies' => array(), 'version' => '676b9bea0a92d4856b5c');
