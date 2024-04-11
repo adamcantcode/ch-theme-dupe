ef3c53252969b3945f39d0774d811a3ab86ff391
amacaulay <adam.macaulay@charliehealth.com>
2024-04-11 10:21:09 -0400

    handle admin

--- wp-content/themes/charliehealth/build/blocks/story/story.php
+++ wp-content/themes/charliehealth/build/blocks/story/story.php
@@ -1,4 +1,4 @@
-<div id="story" class="max-w-[425px] mx-auto opacity-0">
+<div id="story" class="max-w-[425px] mx-auto<?= !is_admin() ? ' opacity-0' : ''; ?>">
   <h2 class="text-center text-white leading-[140%]">
     <span class="relative z-20 inline-block px-base5-2">
       <div class="absolute w-full origin-left scale-x-0 -translate-x-1/2 -translate-y-1/2 rounded-md highlight-bg-js h-5/6 -z-10 bg-lavender-300 px-base5-2 left-1/2 top-1/2"></div>Connecting
