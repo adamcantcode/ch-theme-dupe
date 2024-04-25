cfecefa313a93e24cc1158c346263d28176fd580
amacaulay <adam.macaulay@charliehealth.com>
2024-04-25 14:21:25 -0400

    new block

---
+++ wp-content/themes/charliehealth/blocks/testimonials-videos/index.js
@@ -0,0 +1,116 @@
+import './index.css';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// GSAP
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Bakground color change
+  let sectionBg = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.section-bg-js-cta',
+      start: 'top 70%',
+      endTrigger: '.pin-cta-js-motion',
+      end: 'top 30%',
+      scrub: true,
+      // markers: true,
+    },
+  });
+  sectionBg.fromTo(
+    '.section-bg-js',
+    {
+      background:
+        'linear-gradient(180deg, rgba(247,245,241,1) 0%, rgba(143,146,205,0) 100%)',
+    },
+    {
+      background:
+        'linear-gradient(180deg,rgba(247,245,241,1) 0%, rgba(143,146,205,1) 100%)',
+    }
+  );
+});
+
+// Init settings for first video
+const firstVideo = document.querySelector('.careers-video-js');
+if (firstVideo) {
+  var player = new Vimeo.Player(firstVideo);
+
+  player.play();
+}
+
+// on load, init swiper
+window.addEventListener('load', () => {
+  var unmuted = false;
+  var swiper = new Swiper('.swiper.swiper-careers-testimonial', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    slideToClickedSlide: true,
+    speed: 1000,
+    loop: false,
+    breakpoints: {
+      1024: {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+      },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next-testimonial',
+      prevEl: '.swiper-button-prev-testimonial',
+    },
+    on: {
+      // fix for slider end glitch
+      reachEnd: function () {
+        this.snapGrid = [...this.slidesGrid];
+        setTimeout(() => {
+          document.querySelector('.swiper-button-next-testimonial').click();
+          clearTimeout();
+        }, 1);
+      },
+      slideChange: function () {
+        if (firstVideo) {
+          const currentSlide =
+            swiper.slides[swiper.activeIndex].querySelector('.careers-video-js');
+          const currentPlayer = new Vimeo.Player(currentSlide);
+
+          // Pause all videos
+          swiper.slides.forEach((slide) => {
+            slide = slide.querySelector('.careers-video-js');
+            if (slide) {
+              const player = new Vimeo.Player(slide);
+              player.pause();
+            }
+          });
+
+          // Play current slide video, unmute
+          currentPlayer.play();
+          if (unmuted) {
+            currentPlayer.setVolume(1);
+          }
+        }
+      },
+    },
+  });
+
+  // Handle first interaction/unmute
+  swiper.slides.forEach((slide) => {
+    slide = slide.querySelector('.careers-video-js');
+    if (slide) {
+      const player = new Vimeo.Player(slide);
+      player.on('volumechange', function (data) {
+        if (data.volume !== 0) {
+          if (!unmuted) {
+            player.setCurrentTime(0);
+          }
+          unmuted = true;
+        }
+      });
+    }
+  });
+});
---
+++ wp-content/themes/charliehealth/build/blocks/testimonials-videos/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '45d306f1df1dfd658492');
