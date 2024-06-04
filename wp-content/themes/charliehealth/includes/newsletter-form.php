ba329d4b4b9d810b594ebe708859a284d7eff923
amacaulay <adam.macaulay@charliehealth.com>
2024-06-04 16:48:50 -0400

    remove

--- wp-content/themes/charliehealth/includes/newsletter-form.php
+++ wp-content/themes/charliehealth/includes/newsletter-form.php
@@ -1,14 +1,12 @@
 <form id="iterable_optin" action="//links.iterable.com/lists/publicAddSubscriberForm?publicIdString=f399c731-e9b4-4208-9766-75bbca297d9a" method="POST" class="flex flex-col w-full lg:flex-row gap-base5-2 email">
-  <input type="text" name="email" onfocus="if(this.value===this.defaultValue){this.value='';}" onblur="if(this.value===''){this.value=this.defaultValue;}" placeholder="Email" class="w-full bg-white rounded-md px-base5-3 py-base5-2 text-primary placeholder:text-grey-deactivated">
+  <input type="email" name="email" onfocus="if(this.value===this.defaultValue){this.value='';}" onblur="if(this.value===''){this.value=this.defaultValue;}" placeholder="Email" class="w-full bg-white rounded-md px-base5-3 py-base5-2 text-primary placeholder:text-grey-deactivated">
   <input type="submit" value="Sign Up" class="cursor-pointer ch-button button-primary px-base5-3 py-base5-2 text-[16px]">
 </form>
-<div id="response-message" style="display: none; color: green;">Thank you for signing up!</div>
-<div id="error-message" style="display: none; color: red;">Submission failed. Please try again.</div>
+<h4 id="responseMessage" class="noshow">Thank you for signing up!</h4>
 <script>
   document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('iterable_optin');
-    var responseMessage = document.getElementById('response-message');
-    var errorMessage = document.getElementById('error-message');
+    var responseMessage = document.getElementById('responseMessage');
 
     form.addEventListener('submit', function(event) {
       event.preventDefault();
@@ -19,19 +17,15 @@
       xhr.open('POST', form.action, true);
       xhr.onload = function() {
         if (xhr.status >= 200 && xhr.status < 400) {
-          responseMessage.style.display = 'block';
-          errorMessage.style.display = 'none';
+          form.remove();
+          responseMessage.classList.remove('noshow');
           form.reset();
         } else {
-          errorMessage.style.display = 'block';
-          responseMessage.style.display = 'none';
           console.error('Submission failed:', xhr.statusText);
         }
       };
 
       xhr.onerror = function() {
-        errorMessage.style.display = 'block';
-        responseMessage.style.display = 'none';
         console.error('Request failed');
       };
 
