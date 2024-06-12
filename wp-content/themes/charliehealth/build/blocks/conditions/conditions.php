81b59002d604d6d46ae83d510f44f40ecda45bc6
amacaulay <adam.macaulay@charliehealth.com>
2024-06-12 17:26:35 -0400

    conditions

--- wp-content/themes/charliehealth/build/blocks/conditions/conditions.php
+++ wp-content/themes/charliehealth/build/blocks/conditions/conditions.php
@@ -1,4 +1,5 @@
 <?php
+$block = get_field('block_row');
 if (!empty($block['backgroundColor'])) {
   $background = 'bg-' . $block['backgroundColor'];
   if ($background === 'bg-darker-blue') {
@@ -11,21 +12,34 @@ if (!empty($block['backgroundColor'])) {
   $inverted = true;
 }
 ?>
-<div class="rounded-md <?= $background; ?>">
-  <div class="grid items-center lg:grid-cols-[1fr_1.25fr] lg:py-base5-10 lg:px-base5-8 px-base5-4 py-base5-4 gap-base5-4">
-    <InnerBlocks />
-    <div>
-      <p class="text-h4-base mb-base5-5">Other related areas of care may include</p>
-      <?php if (have_rows('links')) :  ?>
-        <div class="flex flex-wrap gap-base5-3">
-          <?php while (have_rows('links')) : the_row(); ?>
-            <?php $link = get_sub_field('link'); ?>
-            <div class="lg:inline-block flex w-full lg:w-[unset]">
-              <a href="<?= $link['url']; ?>" class="ch-button button-secondary <?= $inverted ? 'inverted' : ''; ?>" target="<?= $link['target'] ?>"><?= $link['title']; ?></a>
-            </div>
-          <?php endwhile; ?>
-        </div>
-      <?php endif; ?>
+<?php if (!$block) : ?>
+  <div class="rounded-md <?= $background; ?>">
+    <div class="grid items-center lg:grid-cols-[1fr_1.25fr] lg:py-base5-10 lg:px-base5-8 px-base5-4 py-base5-4 gap-base5-4">
+      <InnerBlocks />
+      <div>
+        <p class="text-h4-base mb-base5-5">Other related areas of care may include</p>
+        <?php if (have_rows('links')) :  ?>
+          <div class="flex flex-wrap gap-base5-3">
+            <?php while (have_rows('links')) : the_row(); ?>
+              <?php $link = get_sub_field('link'); ?>
+              <div class="lg:inline-block flex w-full lg:w-[unset]">
+                <a href="<?= $link['url']; ?>" class="ch-button button-secondary <?= $inverted ? 'inverted' : ''; ?>" target="<?= $link['target'] ?>"><?= $link['title']; ?></a>
+              </div>
+            <?php endwhile; ?>
+          </div>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
-</div>
+<?php else : ?>
+  <?php if (have_rows('links')) :  ?>
+    <div class="flex flex-wrap justify-center gap-base5-3">
+      <?php while (have_rows('links')) : the_row(); ?>
+        <?php $link = get_sub_field('link'); ?>
+        <div class="lg:inline-block flex w-full lg:w-[unset]">
+          <a href="<?= $link['url']; ?>" class="ch-button button-secondary <?= $inverted ? 'inverted' : ''; ?>" target="<?= $link['target'] ?>"><?= $link['title']; ?></a>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  <?php endif; ?>
+<?php endif; ?>
