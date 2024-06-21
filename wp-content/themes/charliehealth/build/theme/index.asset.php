be1b749d0f5152771e4b3862f04c80ca00c0d802
amacaulay <adam.macaulay@charliehealth.com>
2024-06-21 15:01:52 -0400

    sections 2-3

--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'dbc069e335437b94e8bf');
+<?php return array('dependencies' => array(), 'version' => 'e6289909956026093f8a');
--- wp-content/themes/charliehealth/single-locations.php
+++ wp-content/themes/charliehealth/single-locations.php
@@ -2,23 +2,183 @@
 
 <section class="section">
   <div class="container-md">
-    <h1>Online therapy in <mark style="background-color:#DAC1FB">Alaska</mark></h1>
+    <h1 class="text-center">Online therapy in <mark style="background-color:#DAC1FB">Alaska</mark></h1>
     <div class="flex flex-col lg:flex-row gap-sp-4 lg:items-start items-stretch md:w-[unset] w-full justify-center">
       <a href="/form" class="ch-button button-primary" target="_self">Get Started</a>
       <a href="tel:+18668052001" class="ch-button button-secondary" target="_self">1 (866) 805-2001</a>
     </div>
   </div>
 </section>
-<section class="section">
+<section class="section bg-grey-cool">
   <div class="container-md">
+    <div class="grid lg:grid-cols-[4fr_2fr_4fr] justify-items-center gap-base5-4 mb-base5-10">
+      <div class="flex opacity-50 gap-base5-3">
+        <svg width="41" height="41" viewBox="0 0 41 41" fill="none" xmlns="http://www.w3.org/2000/svg" class="flex-grow flex-shrink-0 basis-auto">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M6.9364 28.333C11.2617 35.8247 20.8413 38.3915 28.333 34.0662C35.8247 29.7409 38.3915 20.1613 34.0662 12.6696C29.7409 5.17792 20.1613 2.61107 12.6696 6.9364C5.17792 11.2617 2.61107 20.8413 6.9364 28.333ZM3.54447 30.2913C8.95135 39.6563 20.9263 42.865 30.2913 37.4581C39.6563 32.0512 42.865 20.0763 37.4581 10.7113C32.0512 1.34627 20.0763 -1.86242 10.7113 3.54447C1.34627 8.95135 -1.86242 20.9263 3.54447 30.2913Z" fill="#161A3D" />
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M18.543 22.4596V10.7096H22.4596V22.4596H18.543Z" fill="#161A3D" />
+          <path d="M18.543 26.3763H22.4596V30.293H18.543V26.3763Z" fill="#161A3D" />
+        </svg>
+        <h2 class="text-h3-base">Oregon ranks as the worst in the U.S. when it comes to youth mental health illness prevalence and access to care, according to Mental Health America.</h2>
+      </div>
+      <div>
+        <div class="w-[2px] h-full bg-primary"></div>
+      </div>
+      <div class="flex gap-base5-3">
+        <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg" class="flex-grow flex-shrink-0 basis-auto">
+          <path d="M31.6254 15.7893L29.4569 13.6208L17.5917 25.4861L12.4361 20.3305L10.2677 22.499L17.5944 29.8257L19.7628 27.6572L19.7601 27.6545L31.6254 15.7893Z" fill="#161A3D" />
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M41.6484 20.9141C41.6484 32.2704 32.4423 41.4766 21.0859 41.4766C9.72958 41.4766 0.523438 32.2704 0.523438 20.9141C0.523438 9.55771 9.72958 0.351562 21.0859 0.351562C32.4423 0.351562 41.6484 9.55771 41.6484 20.9141ZM38.7109 20.9141C38.7109 30.6481 30.82 38.5391 21.0859 38.5391C11.3519 38.5391 3.46094 30.6481 3.46094 20.9141C3.46094 11.18 11.3519 3.28906 21.0859 3.28906C30.82 3.28906 38.7109 11.18 38.7109 20.9141Z" fill="#161A3D" />
+        </svg>
+        <h2 class="text-h3-base">Nearly half of Charlie Health clients in Arizona have less anxiety upon completing treatment</h2>
+      </div>
+    </div>
+    <div class="flex flex-col lg:flex-row gap-sp-4 lg:items-start items-stretch md:w-[unset] w-full justify-center">
+      <a href="/form" class="ch-button button-primary" target="_self">Get Started</a>
+      <a href="tel:+18668052001" class="ch-button button-secondary" target="_self">1 (866) 805-2001</a>
+    </div>
+  </div>
+  </div>
+</section>
+<section class="bg-darker-blue section [&_*]:text-white">
+  <div class="container">
     <div class="acf-innerblocks-container">
-      <h1>Online therapy in <mark style="background-color:#DAC1FB">Alaska</mark></h1>
-      <div class="flex flex-col lg:flex-row gap-sp-4 lg:items-start items-stretch md:w-[unset] w-full justify-center">
-        <a href="/form" class="ch-button button-primary" target="_self">Get Started</a>
-        <a href="tel:+18668052001" class="ch-button button-secondary" target="_self">1 (866) 805-2001</a>
+      <div class="grid lg:grid-cols-[4fr_1fr_7fr] gap-x-sp-5 iop-overview">
+        <div>
+          <h2>What is <span class="rounded-md bg-primary-100 px-sp-4 py-[4px]">IOP?</span></h2>
+          <p>Intensive outpatient programming (IOP) is a level of behavioral healthcare that is typically more structured and intensive than traditional weekly therapy.</p>
+          <p>The goal of IOP is to provide individuals with the support and treatment they need to manage their mental health issues and achieve lasting recovery, while still allowing them to maintain their daily routines and responsibilities.</p>
+        </div>
+        <div></div>
+        <div class="mt-sp-5 lg:mt-0">
+          <div class="iop-overview-accordion">
+            <div class="accordion">
+              <div class="wrapper">
+                <div class="accordion-item-iop bg-primary-100 rounded-[6px] mb-sp-2 lg:mb-sp-4">
+                  <div class="flex justify-between cursor-pointer accordion-header-iop py-[11px] px-sp-6">
+                    <h3 class="mb-0">Curated groups</h3>
+                    <div class="flex items-center ml-sp-5 toggle">
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 11.0001H22.9984V13.0001H1V11.0001Z" fill="white"></path>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.999 23.0001V1.00171H12.999V23.0001H10.999Z" fill="white" class="vertical"></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="overflow-hidden transition-all duration-500 ease-in-out accordion-content max-h-0" style="">
+                    <div class="px-base5-5 pb-base5-2">
+                      <p>By bringing young people with similar mental health challenges together, we’re able to create group environments where clients can learn how to build connections and foster long-term healing.</p>
+                      <p>
+                        <a href="https://www.charliehealth.com/intensive-outpatient-iop/groups" class="no-underline" aria-label="By bringing young people with similar mental health challenges together, we’re able to create group environments where clients can learn how to build connections and foster long-term healing Learn More" uw-rm-vague-link-id="https://www.charliehealth.com/intensive-outpatient-iop/groups$learn more" data-uw-rm-vglnk="" data-uw-rm-brl="PR" data-uw-original-href="https://www.charliehealth.com/intensive-outpatient-iop/groups">Learn More<span class="inline-block align-middle ml-sp-2">
+                            <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="white"></path>
+                            </svg>
+                          </span>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item-iop bg-primary-100 rounded-[6px] mb-sp-2 lg:mb-sp-4">
+                  <div class="flex justify-between cursor-pointer accordion-header-iop py-[11px] px-sp-6">
+                    <h3 class="mb-0">Individual therapy</h3>
+                    <div class="flex items-center ml-sp-5 toggle">
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 11.0001H22.9984V13.0001H1V11.0001Z" fill="white"></path>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.999 23.0001V1.00171H12.999V23.0001H10.999Z" fill="white" class="vertical"></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="overflow-hidden transition-all duration-500 ease-in-out accordion-content max-h-0">
+                    <div class="px-base5-5 pb-base5-2">
+                      <p>One-on-one connections are critical to the IOP model, which is why each client’s unique treatment plan includes a primary therapist for weekly individual sessions.</p>
+                      <p>
+                        <a href="https://www.charliehealth.com/intensive-outpatient-iop/individual-therapy" class="no-underline" aria-label="One-on-one connections are critical to the IOP model, which is why each client’s unique treatment plan includes a primary therapist for weekly individual sessions Learn More" uw-rm-vague-link-id="https://www.charliehealth.com/intensive-outpatient-iop/individual-therapy$learn more" data-uw-rm-vglnk="" data-uw-rm-brl="PR" data-uw-original-href="https://www.charliehealth.com/intensive-outpatient-iop/individual-therapy">Learn More<span class="inline-block align-middle ml-sp-2">
+                            <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="white"></path>
+                            </svg>
+                          </span>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="accordion-item-iop bg-primary-100 rounded-[6px] mb-sp-2 lg:mb-sp-4">
+                  <div class="flex justify-between cursor-pointer accordion-header-iop py-[11px] px-sp-6">
+                    <h3 class="mb-0">Family therapy</h3>
+                    <div class="flex items-center ml-sp-5 toggle">
+                      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M1 11.0001H22.9984V13.0001H1V11.0001Z" fill="white"></path>
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.999 23.0001V1.00171H12.999V23.0001H10.999Z" fill="white" class="vertical"></path>
+                      </svg>
+                    </div>
+                  </div>
+                  <div class="overflow-hidden transition-all duration-500 ease-in-out accordion-content max-h-0">
+                    <div class="px-base5-5 pb-base5-2">
+                      <p><span style="font-weight: 400;">The #1 predictor of success in IOP is family involvement. Our virtual intensive outpatient program’s family therapy component teaches communication and coping skills to create a more supportive home environment both during and after treatment.</span></p>
+                      <p>
+                        <a href="https://www.charliehealth.com/intensive-outpatient-iop/family-therapy" class="no-underline" aria-label=" Our virtual intensive outpatient program’s family therapy component teaches communication and coping skills to create a more supportive home environment both during and after treatment Learn More" uw-rm-vague-link-id="https://www.charliehealth.com/intensive-outpatient-iop/family-therapy$learn more" data-uw-rm-vglnk="" data-uw-rm-brl="PR" data-uw-original-href="https://www.charliehealth.com/intensive-outpatient-iop/family-therapy">Learn More<span class="inline-block align-middle ml-sp-2">
+                            <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="white"></path>
+                            </svg>
+                          </span>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
+  <script>
+    window.addEventListener('DOMContentLoaded', () => {
+      const accordionItems = document.querySelectorAll('.accordion-item-iop');
+      let openAccordionItem = null;
+
+      function closeAccordion() {
+        if (openAccordionItem) {
+          openAccordionItem.classList.remove('active');
+          const accordionContent = openAccordionItem.nextElementSibling;
+          accordionContent.style.maxHeight = null;
+          openAccordionItem = null;
+        }
+      }
+
+      function toggleAccordion() {
+        if (this === openAccordionItem) {
+          closeAccordion();
+        } else {
+          closeAccordion();
+          this.classList.add('active');
+          const accordionContent = this.nextElementSibling;
+          accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+          openAccordionItem = this;
+        }
+      }
+
+      accordionItems.forEach((item) => {
+        const accordionHeader = item.querySelector('.accordion-header-iop');
+        accordionHeader.addEventListener('click', toggleAccordion);
+      });
+
+      const triggerAccordion = () => {
+        if (window.innerWidth > 1024) {
+          document.querySelector('.accordion-header-iop').click();
+        }
+      };
+
+      window.addEventListener('resize', function() {
+        if (!openAccordionItem) {
+          triggerAccordion();
+        } else {
+          closeAccordion();
+        }
+      });
+
+      triggerAccordion();
+    });
+  </script>
 </section>
 
 <?php get_footer(); ?>
