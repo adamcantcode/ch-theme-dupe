20d5547b3ec03e5aa241a993fc3f3cfcaf66eec5
amacaulay <adam.macaulay@charliehealth.com>
2024-06-26 10:16:43 -0400

    revert form script and field ids pls

--- wp-content/themes/charliehealth/page-form.php
+++ wp-content/themes/charliehealth/page-form.php
@@ -11,23 +11,24 @@ Template Post Type: page
   <div class="container">
     <div class="grid grid-cols-1 lg:grid-cols-2 gap-sp-8">
       <div>
-        <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/self_serve_admissions"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/self_serve_admissions" title="Online Form">Online Form - [PROD] Charlie Health Intake Form</a></noscript>
+      <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/self_serve_admissions_copy_2"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/self_serve_admissions_copy_2" title="Online Form">Online Form - UPDATE TESTING [PROD] Charlie Health Intake Form</a></noscript>
         <script>
           document.addEventListener("DOMContentLoaded", function() {
             const newDisclaimerText = 'We may employ third-party tools to analyze usage data on our website, including your submission of this form. We make reasonable efforts to obscure or de-identify protected health information from our analytics providers whenever feasible.'
             const disclaimerContainer = document.querySelectorAll('.field-auto-capture')[0];
-            const progressBar = document.querySelector('#fsSubmit5700521');
+            const progressBar = document.querySelector('#fsSubmit5831039');
 
             progressBar.insertAdjacentElement('afterend', disclaimerContainer);
             disclaimerContainer.style.display = 'none';
             disclaimerContainer.style.padding = "0";
             disclaimerContainer.querySelector('.field-auto-capture__message p').innerHTML = newDisclaimerText;
+            disclaimerContainer.querySelector('.field-auto-capture__message p').style.fontSize = "12px";
             disclaimerContainer.querySelector('.field-auto-capture__message p').style.lineHeight = 1.1;
             disclaimerContainer.querySelector('.field-auto-capture__message p').style.textAlign = "left";
 
             document.querySelector('.fsPagination').addEventListener('click', function() {
               setTimeout(() => {
-                if (document.querySelector('#fsPage5700521-2').classList.contains('fsHiddenPage')) {
+                if (document.querySelector('#fsPage5831039-2').classList.contains('fsHidden')) {
                   setTimeout(() => {
                     disclaimerContainer.style.display = 'none';
                   }, 300);
@@ -40,18 +41,18 @@ Template Post Type: page
             const originalElement = document.querySelector('.field-auto-capture');
             const clonedElement = originalElement.cloneNode(true);
 
-            clonedElement.classList.remove(clonedElement.classList);
+            clonedElement.className = '';
 
             originalElement.parentNode.insertBefore(clonedElement, originalElement.nextSibling);
 
             const clonedChildElement = clonedElement.querySelector('.field-auto-capture__message__text');
-            clonedChildElement.classList.remove(clonedChildElement.classList);
+            clonedChildElement.className = '';
 
             clonedChildElement.innerText = "By entering your phone number and email address in this form, you agree to receive automated text messages and emails from us. Standard message and data rates may apply.";
 
             document.querySelector('.fsPagination').addEventListener('click', function() {
               setTimeout(() => {
-                if (document.querySelector('#fsPage5700521-3').classList.contains('fsHiddenPage')) {
+                if (document.querySelector('#fsPage5831039-4').classList.contains('fsHidden')) {
                   setTimeout(() => {
                     clonedElement.style.display = 'none';
                   }, 300);
@@ -88,7 +89,7 @@ Template Post Type: page
 
             const storedEmail = sessionStorage.getItem('introQuestionEmail');
             const textField = document.getElementById('field162592077');
-            
+
             if (storedEmail && textField) {
               waitForElement("#fsPage5700521-3", (element) => {
 
