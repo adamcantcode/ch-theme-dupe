4dc245cc9564860cb6ade72c0d9351074a1dc7d3
amacaulay <adam.macaulay@charliehealth.com>
2024-07-03 12:37:24 -0400

    center

--- wp-content/themes/charliehealth/build/blocks/hero/hero.php
+++ wp-content/themes/charliehealth/build/blocks/hero/hero.php
@@ -32,7 +32,7 @@ $icon           = get_field('hero_icon');
       <?php if ($heroImage) : ?>
         <img src="<?= $heroImage['sizes']['featured-large'] ?: placeHolderImage(600, 400); ?>" alt="<?= $heroImage['alt'] ?: 'Placeholder image'; ?>" class="block object-cover object-top rounded-lg max-h-52 md:max-h-none nolazy lg:noshow mb-sp-4 hero-image-container">
       <?php endif; ?>
-      <div class="flex items-start gap-sp-4 mb-sp-12 mobile-hero-sub">
+      <div class="flex items-center gap-sp-4 mb-sp-12 mobile-hero-sub">
         <?php if ($icon) : ?>
           <img src="<?= site_url('/wp-content/themes/charliehealth/resources/images/logos/shield-darkest-blue.svg'); ?>" alt="Charlie Health Shield" class="w-10 noshow lg:block mt-base5-1">
         <?php endif; ?>
