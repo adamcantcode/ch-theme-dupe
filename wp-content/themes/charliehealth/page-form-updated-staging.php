fc6f2ccb1438db4aaeb4f73a2096e16a33ede5cb
amacaulay <adam.macaulay@charliehealth.com>
2024-07-08 10:36:07 -0400

    form test template

--- wp-content/themes/charliehealth/page-form-updated-staging.php
+++ wp-content/themes/charliehealth/page-form-updated-staging.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Self-serve admissions [STAGING]
+Template Name: Form [TESTING]
 Template Post Type: page
 */
 ?>
@@ -9,8 +9,14 @@ Template Post Type: page
 
 <div class="section">
   <div class="container">
-    <div class="grid grid-cols-1">
-      <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/charlie_health_pre_admission_copy"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/charlie_health_pre_admission_copy" title="Online Form">Online Form - [Staging] Charlie Health Intake Form</a></noscript>
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-sp-8">
+      <div>
+        <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/self_serve_admissions_copy_3"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/self_serve_admissions_copy_3" title="Online Form">Online Form - [TEST] Charlie Health Intake Form - New Questions</a></noscript>
+        <p>If you’d prefer to speak with our team directly, please call <a href="tel:+18664848218">1 (866) 484-8218</a></p>
+      </div>
+      <div class="grid items-center">
+        <img src="https://www.charliehealth.com/wp-content/uploads/2023/11/Device_Laptop.png.webp" alt="Illustration of Charlie Health client using laptop for Virtual IOP Therapy">
+      </div>
     </div>
   </div>
 </div>
