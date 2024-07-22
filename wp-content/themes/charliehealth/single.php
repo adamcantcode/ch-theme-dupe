6f13b450ce3f26e7cfb952a9e2b0dd7f606ec6ba
amacaulay <adam.macaulay@charliehealth.com>
2024-07-22 17:58:04 -0400

    script logic

--- wp-content/themes/charliehealth/single.php
+++ wp-content/themes/charliehealth/single.php
@@ -323,70 +323,143 @@ $subhead = get_field('subhead', 'option');
     </div>
   </div>
 </div>
-<script>
-  window.addEventListener('DOMContentLoaded', () => {
-    // Function to create a cookie
-    function createCookie(name, value, days) {
-      var expires = '';
-      if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-        expires = '; expires=' + date.toUTCString();
-      }
-      document.cookie = name + '=' + value + expires + '; path=/';
-    }
-
-    // Function to check if the cookie exists
-    function getCookie(name) {
-      var nameEQ = name + '=';
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        while (cookie.charAt(0) === ' ') {
-          cookie = cookie.substring(1, cookie.length);
+<div id="popupScripts">
+  <script>
+    window.addEventListener('DOMContentLoaded', () => {
+      // Function to create a cookie
+      function createCookie(name, value, days) {
+        var expires = '';
+        if (days) {
+          var date = new Date();
+          date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+          expires = '; expires=' + date.toUTCString();
         }
-        if (cookie.indexOf(nameEQ) === 0) {
-          return cookie.substring(nameEQ.length, cookie.length);
-        }
+        document.cookie = name + '=' + value + expires + '; path=/';
       }
-      return null;
-    }
-
-    // Function to handle the scroll event
-    function handleScroll() {
-      var scrollPosition =
-        window.pageYOffset || document.documentElement.scrollTop;
-      var windowHeight = window.innerHeight;
-      var scrollThreshold = windowHeight * 2.5; // Adjust this value as needed
-
-      // Check if the user has scrolled past the threshold and the cookie doesn't exist
-      if (scrollPosition > scrollThreshold && !getCookie('newsletter_popup')) {
-        // Change the class of an element
-        var modal = document.getElementById('newsletterPopupBlogPost');
-        modal.classList.toggle('modal-fade');
-
-        // Create the cookie to prevent further pop-ups
-        createCookie('newsletter_popup', 'true', 1);
-
-        modal.addEventListener('click', (event) => {
-          if (event.target.id === 'newsletterPopupBlogPost') {
-            modal.classList.toggle('modal-fade');
+  
+      // Function to check if the cookie exists
+      function getCookie(name) {
+        var nameEQ = name + '=';
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i];
+          while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
           }
-        });
-
-        const closeButton = modal.querySelector('.modal-close');
-
-        closeButton.addEventListener('click', () => {
-          modal.classList.toggle('modal-fade');
-        });
-
-        // Remove the scroll event listener
-        window.removeEventListener('scroll', handleScroll);
+          if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+          }
+        }
+        return null;
       }
-    }
-
-    // Attach the scroll event listener
-    window.addEventListener('scroll', handleScroll);
-  });
-</script>
+  
+      // Function to handle the scroll event
+      function handleScroll() {
+        var scrollPosition =
+          window.pageYOffset || document.documentElement.scrollTop;
+        var windowHeight = window.innerHeight;
+        var scrollThreshold = windowHeight * 2.5; // Adjust this value as needed
+  
+        // Check if the user has scrolled past the threshold and the cookie doesn't exist
+        if (scrollPosition > scrollThreshold && !getCookie('newsletter_popup')) {
+          // VWO Launch Campaign
+          window.VWO = window.VWO || [];
+          window.VWO.push(['activate', false, [83], true]);
+  
+          // Change the class of an element
+          var modal = document.getElementById('newsletterPopupBlogPost');
+          modal.classList.toggle('modal-fade');
+  
+          // Create the cookie to prevent further pop-ups
+          createCookie('newsletter_popup', 'true', 1);
+  
+          modal.addEventListener('click', (event) => {
+            if (event.target.id === 'newsletterPopupBlogPost') {
+              modal.classList.toggle('modal-fade');
+            }
+          });
+  
+          const closeButton = modal.querySelector('.modal-close');
+  
+          closeButton.addEventListener('click', () => {
+            modal.classList.toggle('modal-fade');
+          });
+  
+          // Remove the scroll event listener
+          window.removeEventListener('scroll', handleScroll);
+        }
+      }
+  
+      // Attach the scroll event listener
+      window.addEventListener('scroll', handleScroll);
+    });
+  </script>
+  <script>
+    // window.addEventListener('DOMContentLoaded', () => {
+    //   // Function to create a cookie
+    //   function createCookie(name, value, days) {
+    //     var expires = '';
+    //     if (days) {
+    //       var date = new Date();
+    //       date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    //       expires = '; expires=' + date.toUTCString();
+    //     }
+    //     document.cookie = name + '=' + value + expires + '; path=/';
+    //   }
+  
+    //   // Function to check if the cookie exists
+    //   function getCookie(name) {
+    //     var nameEQ = name + '=';
+    //     var cookies = document.cookie.split(';');
+    //     for (var i = 0; i < cookies.length; i++) {
+    //       var cookie = cookies[i];
+    //       while (cookie.charAt(0) === ' ') {
+    //         cookie = cookie.substring(1, cookie.length);
+    //       }
+    //       if (cookie.indexOf(nameEQ) === 0) {
+    //         return cookie.substring(nameEQ.length, cookie.length);
+    //       }
+    //     }
+    //     return null;
+    //   }
+  
+    //   // Function to handle the scroll event
+    //   function handleTiming() {
+    //     // Change the time in milliseconds (20000 ms = 20 seconds)
+    //     var launchTime = 20000;
+  
+    //     // Check if the cookie doesn't exist and enough time has passed
+    //     if (!getCookie('newsletter_popup')) {
+    //       // VWO Launch Campaign
+    //       window.VWO = window.VWO || [];
+    //       window.VWO.push(['activate', false, [83], true]);
+  
+    //       // Change the class of an element
+    //       var modal = document.getElementById('newsletterPopupBlogPost');
+    //       modal.classList.toggle('modal-fade');
+  
+    //       // Create the cookie to prevent further pop-ups
+    //       createCookie('newsletter_popup', 'true', 1);
+  
+    //       modal.addEventListener('click', (event) => {
+    //         if (event.target.id === 'newsletterPopupBlogPost') {
+    //           modal.classList.toggle('modal-fade');
+    //         }
+    //       });
+  
+    //       const closeButton = modal.querySelector('.modal-close');
+  
+    //       closeButton.addEventListener('click', () => {
+    //         modal.classList.toggle('modal-fade');
+    //       });
+    //     }
+    //   }
+  
+    //   // Set a timeout to ensure popup is shown after 20 seconds regardless of scroll
+    //   setTimeout(() => {
+    //     handleTiming();
+    //   }, 20000); // Adjust the time here to match the launchTime in milliseconds
+    // });
+  </script>
+</div>
 <?php get_footer(); ?>
