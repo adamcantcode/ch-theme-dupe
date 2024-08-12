05289486e515c88bfc645da76959180505ad7001
amacaulay <adam.macaulay@charliehealth.com>
2024-08-12 10:31:30 -0400

    arrow

--- wp-content/themes/charliehealth/build/blocks/info-accordion/info-accordion.php
+++ wp-content/themes/charliehealth/build/blocks/info-accordion/info-accordion.php
@@ -4,8 +4,10 @@ if (!empty($block['backgroundColor'])) {
   $background = 'bg-' . $block['backgroundColor'];
 }
 $fontColor = '';
+$fill = '#161A3D';
 if ($background === 'bg-darker-blue' || $background === 'bg-primary-100' || $background === 'bg-primary-200' || $background === 'bg-black-blue') {
   $fontColor = '[&_*]:text-white';
+  $fill = '#ffffff';
 };
 ?>
 <div class="iop-overview-accordion">
@@ -20,8 +22,8 @@ if ($background === 'bg-darker-blue' || $background === 'bg-primary-100' || $bac
               <h3 class="mb-0 font-heading-serif"><?= $title; ?></h3>
               <div class="flex items-center ml-sp-5 toggle">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M1 11.0001H22.9984V13.0001H1V11.0001Z" fill="#161A3D" />
-                  <path fill-rule="evenodd" clip-rule="evenodd" d="M10.999 23.0001V1.00171H12.999V23.0001H10.999Z" fill="#161A3D" class="vertical" />
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M1 11.0001H22.9984V13.0001H1V11.0001Z" fill="<?= $fill; ?>" />
+                  <path fill-rule="evenodd" clip-rule="evenodd" d="M10.999 23.0001V1.00171H12.999V23.0001H10.999Z" fill="<?= $fill; ?>" class="vertical" />
                 </svg>
               </div>
             </div>
