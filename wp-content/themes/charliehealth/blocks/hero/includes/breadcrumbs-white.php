597700c97733a8285ca9b581024d810abcb7f6a3
amacaulay <adam.macaulay@charliehealth.com>
2024-09-12 16:35:16 -0400

    hero

---
+++ wp-content/themes/charliehealth/blocks/hero/includes/breadcrumbs-white.php
@@ -0,0 +1,56 @@
+<?php
+// Get the current page/post ID
+$currentID = get_the_ID();
+
+// Get an array of parent pages, starting with the immediate parent
+$parentPages = get_post_ancestors($currentID);
+$parentPages = array_reverse($parentPages);
+
+// For areas of care and treatment modalities
+if (is_single()) {
+  $postTypeDetails = get_post_type_object(get_post_type());
+  $name = $postTypeDetails->labels->menu_name;
+}
+
+// var_dump($postTypeDetai [&_*]:text-whitels);
+
+if (!empty($parentPages)) : ?>
+  <div class="breadcrumbs mb-sp-5 lg:mb-sp-6 [&_*]:!text-white">
+    <a href="<?= home_url(); ?>">Home</a>
+    <?php
+    foreach ($parentPages as $parent_id) : ?>
+      <span>/</span>
+      <a href="<?= get_permalink($parent_id); ?>"><?= get_the_title($parent_id); ?></a>
+    <?php endforeach; ?>
+
+    <span>/</span>
+    <span><?= get_the_title(); ?></span>
+  </div>
+<?php elseif (empty($parentPages) && !is_single()) : ?>
+  <div class="breadcrumbs mb-sp-5 lg:mb-sp-6 [&_*]:!text-white">
+    <a href="<?= home_url(); ?>">Home</a>
+    <span>/</span>
+    <span><?= get_the_title(); ?></span>
+  </div>
+<?php elseif (is_single()) : ?>
+  <?php
+  switch (get_post_type()) {
+    case 'areas-of-care':
+      $url = site_url('/areas-of-care');
+      break;
+    case 'treatment-modalities':
+      $url = site_url('/treatment-modalities');
+      break;
+    default:
+      $url = site_url();
+      break;
+  }
+  ?>
+  <div class="breadcrumbs mb-sp-5 lg:mb-sp-6 [&_*]:!text-white">
+    <a href="<?= home_url(); ?>">Home</a>
+    <span>/</span>
+    <a href="<?= $url; ?>"><?= $name; ?></a>
+    <span>/</span>
+    <span><?= get_the_title(); ?></span>
+  </div>
+<?php endif; ?>
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'cd67802c9b9a13bd4e98');
+<?php return array('dependencies' => array(), 'version' => '3b91041b949a1593aef1');
