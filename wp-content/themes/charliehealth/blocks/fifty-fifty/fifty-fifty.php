c9989083cf5397b35d21b5f34aaba6d355fdb22e
amacaulay <adam.macaulay@charliehealth.com>
2024-10-07 23:11:34 -0400

    md->lg

--- wp-content/themes/charliehealth/blocks/fifty-fifty/fifty-fifty.php
+++ wp-content/themes/charliehealth/blocks/fifty-fifty/fifty-fifty.php
@@ -21,7 +21,7 @@ switch ($order) {
     break;
 }
 ?>
-<div class="grid items-center grid-cols-1 gap-4 just md:gap-32 md:grid-cols-2 last:mb-0 md:mb-sp-14 mb-sp-12">
+<div class="grid items-center grid-cols-1 gap-4 just lg:gap-32 lg:grid-cols-2 last:mb-0 lg:mb-sp-14 mb-sp-12">
   <?php if ($style === 'slider') : ?>
     <?php if (have_rows('fifty_fifty_slides')) : ?>
       <div class="<?= $imageOrder; ?>">
