52786cfea15c4ca86ace277448396b05df04ae59
amacaulay <adam.macaulay@charliehealth.com>
2024-10-17 16:04:30 -0400

    form templates

---
+++ wp-content/themes/charliehealth/page-form-partnership.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Formstack - Partnership Form
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="section">
+  <div class="container">
+    <h1 class="text-center">Partnershp Referral Portal</h1>
+  </div>
+</div>
+<div class="section">
+  <div class="container-sm">
+    <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/partnerships"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/partnerships" title="Online Form">Online Form - Partnership Form</a></noscript>
+  </div>
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/charliehealth/page-form-prof-referral.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Template Name: Formstack - Charlie Health Professional Referral Form
+Template Post Type: page
+*/
+?>
+
+<?php get_header(); ?>
+
+<div class="section-top">
+  <div class="container">
+    <h1 class="text-center">Partner Referral Portal</h1>
+  </div>
+</div>
+
+<div class="section">
+  <div class="container-sm">
+    <script type="text/javascript" src="https://charliehealth-nrkok.formstack.com/forms/js.php/ch_professional_referral_form"></script><noscript><a href="https://charliehealth-nrkok.formstack.com/forms/ch_professional_referral_form" title="Online Form">Online Form - Charlie Health Professional Referral Form</a></noscript>
+  </div>
+</div>
+
+<?php get_footer(); ?>
