6ccffc18c95e71d656c128bd8a6c54c4c200dfee
amacaulay <adam.macaulay@charliehealth.com>
2024-11-14 17:41:50 -0500

    working bg video

--- wp-content/themes/charliehealth/blocks/referrals-testimonials/referrals-testimonials.php
+++ wp-content/themes/charliehealth/blocks/referrals-testimonials/referrals-testimonials.php
@@ -1,5 +1,62 @@
-<div class="h-[1px] bg-primary relative left-1/2 w-screen -ml-[50vw]"></div>
-<div class="relative ml-[200px] mb-[130px]">
+<div class="h-[1px] bg-primary relative left-1/2 w-screen -ml-[50vw]">
+</div>
+<div class="absolute -left-[155px] -top-base5-10 z-10">
+  <div class="video-mask-container">
+    <div class="video-mask-overlay">
+      <iframe src="https://player.vimeo.com/video/1019966136?background=1&autoplay=1&loop=1" allowfullscreen frameborder="0" class="video-iframe"></iframe>
+    </div>
+
+    <svg width="384" height="376" viewBox="0 0 384 376">
+      <defs>
+        <clipPath id="clip-path-mask" clipPathUnits="objectBoundingBox">
+          <path d="M0.87,0.003C0.93,0.003,0.985,0.057,0.99,0.12C1,0.423,0.964,0.655,0.835,0.812C0.819,0.831,0.801,0.849,0.785,0.866C0.647,0.987,0.51,0.998,0.504,0.999L0.5,1L0.495,0.999C0.486,0.997,0.306,0.983,0.164,0.812C0.033,0.654,-0.019,0.423,0.006,0.12C0.011,0.057,0.068,0.003,0.13,0.003C0.215,0,0.788,0,0.87,0.003Z" />
+        </clipPath>
+      </defs>
+    </svg>
+  </div>
+
+  <style>
+    .video-mask-container {
+      position: relative;
+      width: 384px;
+      height: 376px;
+      overflow: hidden;
+    }
+
+    /* Video Mask Overlay with Clipping */
+    .video-mask-overlay {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      clip-path: url(#clip-path-mask);
+      -webkit-clip-path: url(#clip-path-mask);
+    }
+
+    /* Video iframe styling */
+    .video-iframe {
+      width: 100vw;
+      height: 100vh;
+      max-width: unset;
+      object-fit: cover;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  </style>
+  <script>
+    // Select the element whose parent you want to modify
+    const element = document.querySelector('.video-mask-container').parentElement;
+
+    // Set the parent element to position relative
+    if (element.parentElement) {
+      element.parentElement.style.position = 'relative';
+    }
+  </script>
+</div>
+<div class="relative ml-[300px] mb-[130px]">
   <?php // if (have_rows('testimonials')) : 
   ?>
   <div class="!overflow-visible swiper swiper-referrals-testimonial">
@@ -61,50 +118,3 @@
   <?php // endif; 
   ?>
 </div>
-
-<!-- <div>
-  <div class="video-mask-container">
-    <div class="video-mask-overlay">
-      <iframe src="https://player.vimeo.com/video/1019966136?background=1&autoplay=1&loop=1" allowfullscreen frameborder="0" class="video-iframe"></iframe>
-    </div>
-
-    <svg width="384" height="376" viewBox="0 0 384 376">
-      <defs>
-        <clipPath id="clip-path-mask" clipPathUnits="objectBoundingBox">
-          <path d="M0.87,0.003C0.93,0.003,0.985,0.057,0.99,0.12C1,0.423,0.964,0.655,0.835,0.812C0.819,0.831,0.801,0.849,0.785,0.866C0.647,0.987,0.51,0.998,0.504,0.999L0.5,1L0.495,0.999C0.486,0.997,0.306,0.983,0.164,0.812C0.033,0.654,-0.019,0.423,0.006,0.12C0.011,0.057,0.068,0.003,0.13,0.003C0.215,0,0.788,0,0.87,0.003Z" />
-        </clipPath>
-      </defs>
-    </svg>
-  </div>
-
-  <style>
-    .video-mask-container {
-      position: relative;
-      width: 384px;
-      height: 376px;
-      overflow: hidden;
-    }
-
-    /* Video Mask Overlay with Clipping */
-    .video-mask-overlay {
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      clip-path: url(#clip-path-mask);
-      -webkit-clip-path: url(#clip-path-mask);
-    }
-
-    /* Video iframe styling */
-    .video-iframe {
-      width: 100vw;
-      height: 100vh;
-      object-fit: cover;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    }
-  </style>
-</div> -->
--- wp-content/themes/charliehealth/build/theme/index.asset.php
+++ wp-content/themes/charliehealth/build/theme/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'c64fb621ef3bec3c72be');
+<?php return array('dependencies' => array(), 'version' => '7deadb9a29d609b04ad6');
