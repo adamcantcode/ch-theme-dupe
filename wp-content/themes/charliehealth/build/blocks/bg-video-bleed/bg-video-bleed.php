3a392fe3e7ffe7b2a429c1638fae59c4c36dc2bc
amacaulay <adam.macaulay@charliehealth.com>
2024-11-18 13:31:59 -0500

    pos

--- wp-content/themes/charliehealth/build/blocks/bg-video-bleed/bg-video-bleed.php
+++ wp-content/themes/charliehealth/build/blocks/bg-video-bleed/bg-video-bleed.php
@@ -1,45 +1,35 @@
-<div class="relative bg-primary-black-blue">
-  <div class="section-bg-video">
-    <style>
-      .vimeo-container {
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 33%;
-        height: 100%;
-        z-index: 1;
-        pointer-events: none;
-        overflow: hidden;
-        /* Behind the overlay and content */
-      }
-    </style>
-    <!-- Video container with full width and height -->
-    <div class="vimeo-container">
-      <iframe src="https://player.vimeo.com/video/1019966136?&amp;background=1&amp;autoplay=1&amp;loop=1" allowfullscreen="allowfullscreen" frameborder="0" class="w-screen lg:min-w-[56.25vh] min-w-[177.77vh] lg:h-[56.25vw] h-[177.77vh] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></iframe>
-    </div>
-  </div>
-  <script>
-    // // Get the .section-bg-video element
-    // const bgVideoSection = document.querySelector('.section-bg-video');
-  
-    // if (bgVideoSection) {
-    //   // Get the closest parent <section> element (the one we want to remove)
-    //   const parentSection = bgVideoSection.closest('section');
-  
-    //   if (parentSection) {
-    //     // Move the .section-bg-video outside its current parent section
-    //     parentSection.parentNode.insertBefore(bgVideoSection, parentSection);
-  
-    //     // Now remove the parent section
-    //     parentSection.remove();
-    //   }
-    // }
-  </script>
-  <div>
-    <div class="container">
-      <div class="ml-[36%]">
-        <InnerBlocks />
-      </div>
-    </div>
+<div class="section-bg-video">
+  <style>
+    .vimeo-container {
+      position: absolute;
+      top: -35%;
+      left: -40%;
+      width: 70%;
+      height: 170%;
+      z-index: 1;
+      pointer-events: none;
+      overflow: hidden;
+    }
+  </style>
+  <!-- Video container with full width and height -->
+  <div class="vimeo-container">
+    <iframe src="https://player.vimeo.com/video/1019966136?&amp;background=1&amp;autoplay=1&amp;loop=1" allowfullscreen="allowfullscreen" frameborder="0" class="w-screen lg:min-w-[56.25vh] min-w-[177.77vh] lg:h-[56.25vw] h-[177.77vh] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"></iframe>
   </div>
 </div>
+<script>
+  // // Get the .section-bg-video element
+  // const bgVideoSection = document.querySelector('.section-bg-video');
+
+  // if (bgVideoSection) {
+  //   // Get the closest parent <section> element (the one we want to remove)
+  //   const parentSection = bgVideoSection.closest('section');
+
+  //   if (parentSection) {
+  //     // Move the .section-bg-video outside its current parent section
+  //     parentSection.parentNode.insertBefore(bgVideoSection, parentSection);
+
+  //     // Now remove the parent section
+  //     parentSection.remove();
+  //   }
+  // }
+</script>
