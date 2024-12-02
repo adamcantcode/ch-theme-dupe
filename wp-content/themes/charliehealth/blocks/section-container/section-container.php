7a0be7214bb44e9d8433c7522e99b94349f638ec
amacaulay <adam.macaulay@charliehealth.com>
2024-12-02 07:17:56 -0500

    overflow

--- wp-content/themes/charliehealth/blocks/section-container/section-container.php
+++ wp-content/themes/charliehealth/blocks/section-container/section-container.php
@@ -4,9 +4,12 @@
 include(get_template_directory() . '/helpers/spacing.php');
 
 $containerWidth = get_field('container_width') ? '-' . get_field('container_width') : '';
+if(get_current_blog_id() !== 1) {
+  $blockClasses .= ' overflow-hidden ';
+}
 ?>
 
-<section <?= $anchor; ?>class="overflow-hidden <?= $blockClasses; ?> <?= !empty($block['className']) ? $block['className'] : ''; ?>">
+<section <?= $anchor; ?>class="<?= $blockClasses; ?> <?= !empty($block['className']) ? $block['className'] : ''; ?>">
   <div class="container<?= $containerWidth; ?>">
     <InnerBlocks />
   </div>
