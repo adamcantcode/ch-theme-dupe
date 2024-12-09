5de8ab2e416ad160364639e2599c11c5d470d21b
amacaulay <adam.macaulay@charliehealth.com>
2024-12-08 20:10:01 -0500

    -rm link

--- wp-content/themes/charliehealth/blocks/check-list-items/check-list-items.php
+++ wp-content/themes/charliehealth/blocks/check-list-items/check-list-items.php
@@ -40,10 +40,10 @@
         </div>
       <?php endwhile; ?>
     </div>
-    <a href="#" class="flex items-center justify-center no-underline text-referrals-blue-300 gap-base5-2 hover:text-referrals-blue-300">Explore all<svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="#7E87E4"/>
-</svg>
-</a>
+    <!-- <a href="#" class="flex items-center justify-center no-underline text-referrals-blue-300 gap-base5-2 hover:text-referrals-blue-300">Explore all<svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="#7E87E4" />
+      </svg>
+    </a> -->
   <?php endif; ?>
 <?php else:  ?>
   <?php if (have_rows('services')) : ?>
