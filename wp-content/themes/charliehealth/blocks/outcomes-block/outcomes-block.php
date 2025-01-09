e3f0ede862bfd5856c7dc063488b4310c1e2a0e2
amacaulay <adam.macaulay@charliehealth.com>
2025-01-09 13:02:54 -0500

    color

--- wp-content/themes/charliehealth/blocks/outcomes-block/outcomes-block.php
+++ wp-content/themes/charliehealth/blocks/outcomes-block/outcomes-block.php
@@ -54,7 +54,7 @@
         </svg>
       </div>
       <div class="flex items-center order-1 lg:justify-end lg:order-2">
-        <p class="mb-0 "><a <?php if (!is_admin()) :; ?> href="<?= $linkUrl; ?>" <?php endif; ?> class="no-underline stretched-link text-h4-base hover:text-white"><?= $linkTitle; ?></a></p>
+        <p class="mb-0 "><a <?php if (!is_admin()) :; ?> href="<?= $linkUrl; ?>" <?php endif; ?> class="!no-underline stretched-link text-h4-base !text-white hover:text-white"><?= $linkTitle; ?></a></p>
         <svg width="17" height="14" viewBox="0 0 17 14" fill="none" xmlns="http://www.w3.org/2000/svg" class="ml-sp-4 group-hover:translate-x-[5px] transition-all duration-300">
           <path fill-rule="evenodd" clip-rule="evenodd" d="M10.3431 0.278417L16.7071 6.32784C17.0976 6.69906 17.0976 7.30094 16.7071 7.67216L10.3431 13.7216C9.95262 14.0928 9.31946 14.0928 8.92893 13.7216C8.53841 13.3504 8.53841 12.7485 8.92893 12.3773L13.5858 7.95058H0V6.04942H13.5858L8.92893 1.62273C8.53841 1.25151 8.53841 0.64964 8.92893 0.278417C9.31946 -0.0928058 9.95262 -0.0928058 10.3431 0.278417Z" fill="white" />
         </svg>
