e407a7606d19539f954cbd36cc04d9c516c0ac66
amacaulay <adam.macaulay@charliehealth.com>
2025-04-23 14:12:31 -0400

    offset, text, poster

--- wp-content/themes/charliehealth/blocks/hero-bg-video/hero-bg-video.php
+++ wp-content/themes/charliehealth/blocks/hero-bg-video/hero-bg-video.php
@@ -1,10 +1,12 @@
-<section class="relative w-screen h-screen overflow-hidden">
+<section class="relative w-screen h-screen overflow-hidden" style="height:calc(100vh - 70px); margin-top:70px">
   <video autoplay muted loop playsinline poster="/wp-content/themes/charliehealth/resources/videos/background/poster.png" class="absolute top-0 left-0 z-0 object-cover w-full h-full">
-    <source src="/wp-content/themes/charliehealth/resources/videos/background/Sizzle_rough.mp4" type="video/mp4" />
+    <source src="/wp-content/themes/charliehealth/resources/videos/background/charlie-health-iop.mp4" type="video/mp4" />
   </video>
-  <div class="container relative z-10 flex flex-col items-center justify-center h-full text-center">
-    <h1>Your Headline</h1>
-    <h2>Your Subheading</h2>
-    <p>Some content here</p>
+  <div class="relative z-10 flex flex-col items-center justify-center h-full text-center container-md">
+    <h1 class="text-white font-heading-serif mb-base5-10">Connecting the world to life-saving behavioral health treatment.</h1>
+    <div class="flex flex-col lg:flex-wrap lg:flex-row gap-sp-4 lg:items-start items-stretch md:w-[unset] w-full">
+      <a href="/form" class="ch-button button-primary-ch">Get Started</a>
+      <a href="tel:+19862060414" class="ch-button button-secondary-ch invert">1 (986) 206-0414</a>
+    </div>
   </div>
 </section>
