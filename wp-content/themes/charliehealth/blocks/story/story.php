186507918519606eb70b2d5c511d74fe1be162a4
amacaulay <adam.macaulay@charliehealth.com>
2025-05-06 14:00:09 -0400

    behavioral

--- wp-content/themes/charliehealth/blocks/story/story.php
+++ wp-content/themes/charliehealth/blocks/story/story.php
@@ -6,6 +6,6 @@
       <div class="absolute w-full origin-left scale-x-0 -translate-x-1/2 -translate-y-1/2 rounded-md highlight-bg-js h-5/6 -z-10 bg-pale-blue-300 px-base5-2 left-1/2 top-1/2"></div>world
     </span> to <span class="relative z-20 inline-block color-change-js px-base5-2">
       <div class="absolute w-full origin-left scale-x-0 -translate-x-1/2 -translate-y-1/2 bg-yellow-300 rounded-md highlight-bg-js h-5/6 -z-10 px-base5-2 left-1/2 top-1/2"></div>life-saving
-    </span> mental health treatment
+    </span> behavioral health treatment
   </h2>
 </div>
